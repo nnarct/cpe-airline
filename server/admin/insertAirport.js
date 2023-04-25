57c871aee6a4e5a949ee9f20104880fb299befbe
nnarct <nannapat1551@gmail.com>
2023-04-25 13:36:57 +0700

    feat: create insert airport function

---
+++ server/admin/insertAirport.js
@@ -0,0 +1,22 @@
+import { db } from "../index.js";
+export const insertAirport = (req,res) => {
+  const sqlCheck = `SELECT * FROM airport WHERE IATA = ?`;
+  const sql = "INSERT INTO airport (Name, IATA, State, Province) VALUES (?)";
+  db.query(sqlCheck, [req.body.IATA], (err, response) => {
+    if (err)
+      return res.json({ Error: "Error while checking IATA in db..." });
+    if (response.length > 0)
+      return res.json({ Error: "IATA is already exist." });
+    const values = [
+      req.body.Name,
+      req.body.IATA,
+      req.body.State,
+      req.body.Province,
+    ];
+    db.query(sql, [values], (err, result) => {
+      if (err)
+        return res.json({ Error: "Inserting data error in server..." });
+      return res.json({ Status: "Create new airport successfully! :)" });
+    });
+  });
+};
