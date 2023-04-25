43751ea0a40622d827a65310e6905162ff665b5d
nnarct <nannapat1551@gmail.com>
2023-04-25 14:07:30 +0700

    fix: hotfix

--- server/admin/deleteAirport.js
+++ server/admin/deleteAirport.js
@@ -1,8 +1,8 @@
 import { db } from "../index.js";
 export const deleteAirport = (req, res) => {
-  const sql = `DELETE FROM airport WHERE IATA = ?`;
-  db.query(sql, [req.body.IATA], (err, result) => {
+  const sql = `DELETE FROM airport WHERE AirportID = ?`;
+  db.query(sql, [req.body.id], (err, result) => {
     if (err) return res.json({ Error: "Error while deleting airport..." });
     return res.json({ Status: "Delete airport successfully! :)" });
   });
-}
+};
