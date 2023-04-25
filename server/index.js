56297c26dbf007bfee90963d870a9c057e4e25e2
nnarct <nannapat1551@gmail.com>
2023-04-25 09:49:38 +0700

    feat: beautify app routes

--- server/index.js
+++ server/index.js
@@ -1,10 +1,23 @@
 import express from "express";
 import mysql from "mysql";
 import cors from "cors";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
+
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+
+import { verifyUser, verifyUserRes } from "./users/verifyUser";
+import { register } from "./users/register";
+import { login } from "./users/login";
+import { logout } from "./users/logout";
+
+import { verifyAdmin, verifyAdminRes } from "./admin/verifyAdmin";
+import { registerAdmin } from "./admin/registerAdmin";
+import { editEmployee } from "./admin/editEmployee";
+import { editAirline } from "./admin/editAirline";
+import { loginAdmin } from "./admin/loginAdmin";
+import { logoutAdmin } from "./admin/logoutAdmin";
+import { employeeList } from "./admin/employeeList";
+import { airlineList } from "./admin/airlineList";
 
 const app = express();
 app.use(express.json());
@@ -20,7 +33,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const SALT = 10;
 
-const db = mysql.createConnection({
+export const db = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
@@ -28,259 +41,22 @@ const db = mysql.createConnection({
   port: 4306,
 });
 
-const verifyUser = (req, res, next) => {
-  const userToken = req.cookies.userToken;
+app.get("/", verifyUser, verifyUserRes);
+app.get("/emauth", verifyAdmin, verifyAdminRes);
 
-  if (!userToken) return res.json({ Status: "You are not authenticated" });
-  else {
-    jwt.verify(userToken, "jwt-secret-key", (err, decoded) => {
-      if (err) return res.json({ Error: "Token is not ok" });
-      else {
-        req.firstName = decoded.firstName;
-        next();
-      }
-    });
-  }
-};
+app.post("/register", register);
+app.post("/login", login);
+app.get("/logout", logout);
 
-app.get("/", verifyUser, (req, res) => {
-  return res.json({ Status: "Success", firstName: req.firstName });
-});
+app.post("/admin/register", registerAdmin);
+app.post("/admin/login", loginAdmin);
+app.get("/admin/logout", logoutAdmin);
 
-app.post("/register", (req, res) => {
-  const sqlCheck = `SELECT * FROM user WHERE email = "${req.body.email}"`;
-  const sql =
-    "INSERT INTO user (FirstName, LastName, Email, TelNo, Password) VALUES (?)";
-  db.query(sqlCheck, (err, response) => {
-    if (err) return res.json({ Error: "Error while checking email in db..." });
-    if (response.length > 0)
-      return res.json({ Error: "Email is already exist." });
-    bcrypt.hash(req.body.password.toString(), SALT, (err, hash) => {
-      if (err) return res.json({ Error: "Error while hashing password !" });
-      const values = [
-        req.body.firstName,
-        req.body.lastName,
-        req.body.email,
-        req.body.telNo,
-        hash,
-      ];
-      db.query(sql, [values], (err, result) => {
-        if (err)
-          return res.json({ Error: "Inserting data error in server..." });
-        return res.json({ Status: "Create new user successfully! :)" });
-      });
-    });
-  });
-});
+app.get("/admin/employeeList", employeeList);
+app.get("/admin/airlineList", airlineList);
 
-app.post("/login", (req, res) => {
-  const sql = "SELECT * FROM user WHERE email = ?";
-  db.query(sql, [req.body.email], (err, data) => {
-    if (err) {
-      console.log(err);
-      return res.json({ Error: "Login error in server..." });
-    }
-    if (data.length > 0) {
-      bcrypt.compare(
-        req.body.password.toString(),
-        data[0].Password,
-        (err, response) => {
-          if (err) return res.json({ Error: "Password compare error..." });
-          if (response) {
-            const firstName = data[0].firstName;
-            const token = jwt.sign({ firstName }, "jwt-secret-key", {
-              expiresIn: "1d",
-            });
-            res.cookie("userToken", token);
-            return res.json({ Status: "Successfully login" });
-          } else {
-            return res.json({ Error: "Password does not match..." });
-          }
-        }
-      );
-    } else return res.json({ Error: "Data not found" });
-  });
-});
-
-app.get("/logout", (req, res) => {
-  res.clearCookie("userToken");
-  return res.json({ Status: "Success" });
-});
-
-const verifyAdmin = (req, res, next) => {
-  const admin = req.cookies.admin;
-  if (!admin) return res.json({ Status: "You are not authenticated admin" });
-  else {
-    jwt.verify(admin, "admin-secret-key", (err, decoded) => {
-      if (err) return res.json({ Error: "Admin token is not ok" });
-      else {
-        req.Info = decoded.info;
-        next();
-      }
-    });
-  }
-};
-
-app.get("/emauth", verifyAdmin, (req, res) => {
-  return res.json({ Status: "Success", Info: req.Info });
-});
-
-app.post("/admin/register", (req, res) => {
-  const sqlCheck = `SELECT * FROM employee WHERE username = ?`;
-  const sql =
-    "INSERT INTO employee (FirstName, LastName, username, Email, TelNo, Password, Position) VALUES (?)";
-  db.query(sqlCheck, [req.body.username], (err, response) => {
-    if (err)
-      return res.json({ Error: "Error while checking username in db..." });
-    if (response.length > 0)
-      return res.json({ Error: "Username is already exist." });
-    bcrypt.hash(req.body.password.toString(), SALT, (err, hash) => {
-      if (err) return res.json({ Error: "Error while hashing password !" });
-      const values = [
-        req.body.firstName,
-        req.body.lastName,
-        req.body.username,
-        req.body.email,
-        req.body.TelNo,
-        hash,
-        req.body.position,
-      ];
-      db.query(sql, [values], (err, result) => {
-        if (err)
-          return res.json({ Error: "Inserting data error in server..." });
-        return res.json({ Status: "Create new admin successfully! :)" });
-      });
-    });
-  });
-});
-
-app.post("/system/editEmployee", (req, res) => {
-  const sql =
-    "UPDATE employee SET FirstName = ?, LastName = ?, username = ?, Email = ?, TelNo = ?, Position = ?, AirlineID = ? WHERE EmployeeID =?";
-  db.query(
-    sql,
-    [
-      req.body.FirstName,
-      req.body.LastName,
-      req.body.username,
-      req.body.Email,
-      req.body.TelNo,
-      req.body.Position,
-      req.body.AirlineID,
-      req.body.EmployeeID,
-    ],
-    (err, result) => {
-      if (err) {
-        return res.json({ Error: "Error while editing employee..." });
-      }
-      return res.json({ Status: "Edit employee successfully! :)" });
-    }
-  );
-});
-app.post("/system/editAirline", (req, res) => {
-  const sql =
-    "UPDATE airline SET Name = ?, LogoImage = ?, Link = ? WHERE AirlineID =?";
-  db.query(
-    sql,
-    [req.body.Name, req.body.LogoImage, req.body.Link, req.body.AirlineID],
-    (err, result) => {
-      if (err) {
-        return res.json({ Error: "Error while editing airline..." });
-      }
-      return res.json({ Status: "Edit airline successfully! :)" });
-    }
-  );
-});
-
-app.post("/admin/login", (req, res) => {
-  const sql = "SELECT * FROM employee WHERE username = ?";
-  db.query(sql, [req.body.username], (err, data) => {
-    if (err) {
-      console.log(err);
-      return res.json({ Error: "Login error in server..." });
-    }
-    if (data.length > 0) {
-      bcrypt.compare(
-        req.body.password.toString(),
-        data[0].Password,
-        (err, response) => {
-          if (err) return res.json({ Error: "Password compare error..." });
-          if (response) {
-            const info = data[0];
-            const token = jwt.sign({ info }, "admin-secret-key", {
-              expiresIn: "1d",
-            });
-            res.cookie("admin", token);
-            return res.json({
-              Status: "Successfully login admin",
-              Position: data[0].Position,
-            });
-          } else {
-            return res.json({ Error: "Password does not match..." });
-          }
-        }
-      );
-    } else return res.json({ Error: "Data not found" });
-  });
-});
-
-app.get("/admin/logout", (req, res) => {
-  res.clearCookie("admin");
-  return res.json({ Status: "Success admin" });
-});
-
-app.get("/admin/employeeList", (req, res) => {
-  const sql = "SELECT * FROM employee";
-  db.query(sql, (err, data) => {
-    if (err) {
-      return res.json({ Error: "Select employee list error in server..." });
-    }
-    if (data.length > 0) {
-      // console.log(data);
-      return res.json({
-        Status: "Successfully select employee list",
-        Data: data,
-      });
-    } else {
-      return res.json({ Error: "Employee List not found" });
-    }
-  });
-});
-app.get("/admin/airlineList", (req, res) => {
-  const sql = "SELECT * FROM airline";
-  db.query(sql, (err, data) => {
-    if (err) {
-      console.log(err);
-      return res.json({ Error: "Select airline list error in server..." });
-    }
-    if (data.length > 0) {
-      // console.log(data);
-      return res.json({
-        Status: "Successfully select airline list",
-        Data: data,
-      });
-    } else {
-      return res.json({ Error: "Airline List not found" });
-    }
-  });
-});
-
-// app.post("/api/airline", (req, res) => {
-//   const sql = "SELECT * FROM airline";
-//   db.query(sql, (err, data) => {
-//     if (err) {
-//       return res.json({ Error: "Select airline list error in server..." });
-//     }
-//     if (data.length > 0) {
-//       return res.json({
-//         Status: "Successfully select airline list",
-//         Data: data,
-//       });
-//     } else {
-//       return res.json({ Error: "Airline List not found" });
-//     }
-//   });
-// });
+app.post("/system/editEmployee", editEmployee);
+app.post("/system/editAirline", editAirline);
 
 app.listen(3001, () => {
   console.log("running on port 3001");
