e69d01422b2a06a1c7c5f6180c3668a0183e420f
nnarct <nannapat1551@gmail.com>
2023-04-25 22:38:23 +0700

    ref: extraction component for dashboard content

---
+++ client/src/admin/system/components/content.js
@@ -0,0 +1,7 @@
+export const Content = ({children}) => {
+  return(
+    <>
+     <div className="flex flex-col items-center w-full max-h-calc overflow-y-auto pb-4">
+      {children}</div></>
+  );
+}
---
+++ client/src/admin/system/components/header.js
@@ -0,0 +1,4 @@
+export const Header = ({children}) => {
+  return (<>
+          <h1 className="text-3xl py-4 font-bold">{children}</h1></>);
+}
