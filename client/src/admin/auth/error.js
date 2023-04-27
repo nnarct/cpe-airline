e6b6ce84784a6ba666af353ed17ec0f43c897163
nnarct <nannapat1551@gmail.com>
2023-04-27 16:15:19 +0700

    feat: create error page component:

---
+++ client/src/admin/auth/error.js
@@ -0,0 +1,8 @@
+export const ErrorPage = () => { 
+  return (
+    <div className="w-screen h-screen bg-slate-300 flex flex-col items-center justify-center space-y-5">
+      <h1 className="text-4xl font-bold">404</h1>
+      <h1 className="text-2xl font-semibold">Page Not Found</h1>
+    </div>
+  );
+}
