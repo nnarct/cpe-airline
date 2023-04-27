f7ec746f949224cb4940bc3fc3c7be7cd1375e51
nnarct <nannapat1551@gmail.com>
2023-04-27 16:49:31 +0700

    style: beautify

--- client/src/admin/auth/admin-login.js
+++ client/src/admin/auth/admin-login.js
@@ -1,19 +1,20 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Axios from "axios";
-
+import { AiOutlineEyeInvisible, AiOutlineEye } from "react-icons/ai";
+import { Star } from "../components/star";
 export const LoginAdmin = () => {
   const navigate = useNavigate();
-  const [adminAuth, setAdminAuth] = useState(false);
   const [values, setValues] = useState({
     username: "",
     password: "",
   });
+
+  const [seePass, setSeePass] = useState("password");
   const handleSubmit = () => {
     Axios.post("http://localhost:3001/admin/login", values)
       .then((res) => {
         if (res.data.Status === "Successfully login admin") {
-          setAdminAuth(true);
           if (res.data.Position === "Admin") navigate("/admin");
           if (res.data.Position === "System") navigate("/system");
           else navigate("/error");
@@ -28,51 +29,66 @@ export const LoginAdmin = () => {
     Axios.get("http://localhost:3001/emauth").then((res, err) => {
       if (err) console.log(err); // You are not authenticated
       if (res.data.Status === "Success") {
-        setAdminAuth(true);
         if (res.data.Info.Position === "Admin") navigate("/admin");
         if (res.data.Info.Position === "System") navigate("/system");
-      } else {
-        setAdminAuth(false);
       }
     });
   });
   return (
     <>
-      <div className="w-screen h-screen bg-slate-300 flex flex-col items-center justify-center space-y-5">
-        <h1 className="text-4xl font-bold">Admin Login</h1>
-        <h1 className="text-2xl font-semibold">
-          auth: {adminAuth ? "true" : "false"}
-        </h1>
-
+      <div className="w-screen h-screen flex flex-col items-center justify-center space-y-5 bg-gradient-to-br from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%">
         <form
-          className="bg-white rounded w-56 p-4 space-y-3"
+          className="bg-white rounded w-72 p-6 flex flex-col shadow-xl"
           onSubmit={(e) => {
             e.preventDefault();
             handleSubmit();
           }}
         >
-          <label htmlFor="email">Username</label>
+          <h1 className="text-4xl font-semibold text-center mb-5">
+            Admin Login
+          </h1>
+          <label htmlFor="email">
+            Username <Star />
+          </label>
           <input
+            required
             name="username"
             type="text"
-            className="px-1 w-full"
-            placeholder="username"
+            className="w-full border border-gray-200 rounded mb-4 p-1 focus:ring outline-none"
+            placeholder="Username"
             onChange={(e) => {
               setValues({ ...values, username: e.target.value });
             }}
           />
-          <label htmlFor="password">password</label>
-          <input
-            name="password"
-            type="password"
-            className="px-1 w-full"
-            placeholder="********"
-            onChange={(e) => {
-              setValues({ ...values, password: e.target.value });
-            }}
-          />
+          <label htmlFor="password">
+            Password <Star />
+          </label>
+          <div className="relative">
+            <input
+              required
+              name="password"
+              type={seePass}
+              className="w-full border border-gray-200 rounded mb-4 p-1 focus:ring outline-none"
+              placeholder="********"
+              onChange={(e) => {
+                setValues({ ...values, password: e.target.value });
+              }}
+            />
+            {seePass === "password" ? (
+              <AiOutlineEyeInvisible
+                className="absolute right-3 top-2 text-gray-400 cursor-pointer"
+                onClick={() => setSeePass("text")}
+              />
+            ) : (
+              <AiOutlineEye
+                className="absolute right-3 top-2 text-gray-400 cursor-pointer"
+                onClick={() => setSeePass("password")}
+              />
+            )}
+          </div>
+
           <button
-            className="w-full bg-blue-500 text-white rounded py-1 hover:opacity-40 active:opacity-80"
+            className="w-full bg-blue-500 text-white rounded py-1 hover:bg-blue-700 active:opacity-80 transition linear"
             type="submit"
           >
             Login
