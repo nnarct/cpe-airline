024a51edd319329a2e1e4a4631f99c759b756c73
nnarct <nannapat1551@gmail.com>
2023-04-27 23:24:21 +0700

    feat: add scroll to add airport comp

--- client/src/admin/system/components/addAirport.js
+++ client/src/admin/system/components/addAirport.js
@@ -13,7 +13,6 @@ export const AddAirport = () => {
     if (values.IATA.length !== 3) {
       alert("IATA must be 3 capital characters");
     } else {
-      
       Axios.post("http://localhost:3001/system/insertAirport", values)
         .then((res) => {
           if (res.data.Status === "Create new airport successfully! :)") {
@@ -29,69 +28,75 @@ export const AddAirport = () => {
   };
   return (
     <>
-      <div className="flex flex-col items-center bg-gray-50 border rounded mt-4 p-3">
-        <h1 className="text-xl">Add New Airport</h1>
-        <form
-          className="rounded w-96 flex flex-col p-4 "
-          onSubmit={(e) => {
-            e.preventDefault();
-            handleSubmit();
-          }}
-        >
-          <label htmlFor="name">
-            Airport Name <Star />
-          </label>
-          <input
-            required
-            name="name"
-            type="text"
-            className="mb-2 border rounded p-1"
-            placeholder="Airport Name"
-            onChange={(e) => setValues({ ...values, Name: e.target.value })}
-          />
-          <label htmlFor="iata">
-            IATA
-            <Star />
-          </label>
-          <input
-            required
-            name="iata"
-            type="text"
-            className="mb-2 border rounded p-1"
-            placeholder="IATA"
-            onChange={(e) => setValues({ ...values, IATA: e.target.value.toUpperCase() })}
-          />
-          <label htmlFor="state">
-            State
-            <Star />
-          </label>
-          <input
-            required
-            name="state"
-            type="text"
-            className="mb-2 border rounded p-1"
-            placeholder="State"
-            onChange={(e) => setValues({ ...values, State: e.target.value })}
-          />
-          <label htmlFor="province">
-            Province
-            <Star />
-          </label>
-          <input
-            required
-            name="province"
-            type="text"
-            className="mb-2 border rounded p-1"
-            placeholder="Province"
-            onChange={(e) => setValues({ ...values, Province: e.target.value })}
-          />
-          <button
-            className="bg-blue-500 text-white rounded py-1 px-3 hover:opacity-40  active:opacity-80"
-            type="submit"
+      <div className="min-h-[calc(100vh-300px)]">
+        <div className="flex flex-col items-center bg-gray-50 border rounded mt-4 p-3">
+          <h1 className="text-xl">Add New Airport</h1>
+          <form
+            className="rounded w-96 flex flex-col p-4 "
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleSubmit();
+            }}
           >
-            Add Airport
-          </button>
-        </form>
+            <label htmlFor="name">
+              Airport Name <Star />
+            </label>
+            <input
+              required
+              name="name"
+              type="text"
+              className="mb-2 border rounded p-1"
+              placeholder="Airport Name"
+              onChange={(e) => setValues({ ...values, Name: e.target.value })}
+            />
+            <label htmlFor="iata">
+              IATA
+              <Star />
+            </label>
+            <input
+              required
+              name="iata"
+              type="text"
+              className="mb-2 border rounded p-1"
+              placeholder="IATA"
+              onChange={(e) =>
+                setValues({ ...values, IATA: e.target.value.toUpperCase() })
+              }
+            />
+            <label htmlFor="state">
+              State
+              <Star />
+            </label>
+            <input
+              required
+              name="state"
+              type="text"
+              className="mb-2 border rounded p-1"
+              placeholder="State"
+              onChange={(e) => setValues({ ...values, State: e.target.value })}
+            />
+            <label htmlFor="province">
+              Province
+              <Star />
+            </label>
+            <input
+              required
+              name="province"
+              type="text"
+              className="mb-2 border rounded p-1"
+              placeholder="Province"
+              onChange={(e) =>
+                setValues({ ...values, Province: e.target.value })
+              }
+            />
+            <button
+              className="bg-blue-500 text-white rounded py-1 px-3 hover:opacity-40  active:opacity-80"
+              type="submit"
+            >
+              Add Airport
+            </button>
+          </form>
+        </div>
       </div>
     </>
   );
