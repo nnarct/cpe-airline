180e826aeff36b5bcedd4525ea643304a89bea5a
nnarct <nannapat1551@gmail.com>
2023-04-29 00:20:24 +0700

    feat: custom scroll bar

---
+++ client/src/admin/system/sidebar/sidebar.js
@@ -0,0 +1,45 @@
+import { Menu } from "./sidebarMenu";
+import "./sidebar.css";
+export const Sidebar = ({ setContent, current }) => {
+  return (
+    <>
+      <div className="w-52 bg-sky-900 max-h-calc overflow-y-auto admin-sidebar">
+        <ul className="list-none p-4 space-y-2">
+          <Menu current={current} setContent={setContent} content="Dashboard">
+            Dashboard
+          </Menu>
+          <Menu
+            current={current}
+            setContent={setContent}
+            content="EmployeeList"
+          >
+            Employee List
+          </Menu>
+          <Menu current={current} setContent={setContent} content="AirlineList">
+            Airline List
+          </Menu>
+          <Menu current={current} setContent={setContent} content="AirportList">
+            Airport List
+          </Menu>
+          <Menu current={current} setContent={setContent} content="FlightList">
+            Flight Info
+          </Menu>
+          <Menu current={current} setContent={setContent} content="UserList">
+            User List
+          </Menu>
+          <Menu
+            current={current}
+            setContent={setContent}
+            content="PassengerList"
+          >
+            Passenger List
+          </Menu>
+
+          <Menu current={current} setContent={setContent} content="PlaneList">
+            Plane List
+          </Menu>
+        </ul>
+      </div>
+    </>
+  );
+};
