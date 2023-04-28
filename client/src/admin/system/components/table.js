4766a1f0aa04a73352562dc68b7665ba020f68d4
nnarct <nannapat1551@gmail.com>
2023-04-28 23:07:39 +0700

    style: add css

--- client/src/admin/system/components/table.js
+++ client/src/admin/system/components/table.js
@@ -5,3 +5,40 @@ export const Table = ({ children }) => {
     </>
   );
 };
+export const THead = ({ children }) => {
+  return (
+    <>
+      <thead>
+        <tr>{children}</tr>
+      </thead>
+    </>
+  );
+};
+
+export const Th = ({ children, className, width }) => {
+  return (
+    <>
+      <th
+        className={`p-1 border border-1 border-black whitespace-nowrap ${className} ${width}`}
+      >
+        {children}
+      </th>
+    </>
+  );
+};
+
+export const TBody = ({ children }) => {
+  return (
+    <>
+      <tbody>{children}</tbody>
+    </>
+  );
+};
+
+export const Td = ({ e, className }) => {
+  return (
+    <>
+      <td className={`border px-3 py-2 ${className}`}>{e ? e : "-"}</td>
+    </>
+  );
+};
