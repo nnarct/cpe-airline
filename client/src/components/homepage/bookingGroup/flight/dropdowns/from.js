a35d7069a8df58418a34713cc4ed7e92477cab22
nnarct <nannapat1551@gmail.com>
2023-05-01 23:18:59 +0700

    style: use css components

--- client/src/components/homepage/bookingGroup/flight/dropdowns/from.js
+++ client/src/components/homepage/bookingGroup/flight/dropdowns/from.js
@@ -4,6 +4,7 @@ import { useDetectClickOutside } from "react-detect-click-outside";
 import { FaPlaneDeparture } from "react-icons/fa";
 import { IoIosArrowDown } from "react-icons/io";
 import { Card } from "../card";
+import { DropHead } from "./components/drophead";
 
 export const From = ({ airports, values, setValues }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -38,28 +39,16 @@ export const From = ({ airports, values, setValues }) => {
           isOpen ? "ring ring-blue-500/20 rounded-xl" : ""
         }`}
         onClick={() => {
-          console.log(isOpen);
           setIsOpen(!isOpen);
         }}
       >
-        <div className="flex items-center text-primary">
-          <span className="px-2">
-            <FaPlaneDeparture color="0D3E5E" size={20} />
-          </span>
-          <div className="flex flex-col pl-2">
-            <h1 className="text-xl font-bold line-clamp-1">
-              {toShortName(values.from)} ({toIATA(values.from)})
-            </h1>
-            <span className="text-xs line-clamp-1">
-              {toFullName(values.from)}
-            </span>
-          </div>
-        </div>
-        <div className="px-2">
-          <IoIosArrowDown color="0D3E5E" size={20} />
-        </div>
+        <DropHead
+          icon={<FaPlaneDeparture color="0D3E5E" size={20} />}
+          title={`${toShortName(values.from)} (${toIATA(values.from)})`}
+          subtitle={toFullName(values.from)}
+        />
         {isOpen ? (
-          <Card className="absolute left-0 top-20 z-20 shadow h-auto flex flex-col justify-center">
+          <Card className="absolute left-0 top-20 z-20 h-auto flex flex-col justify-center ring ring-4 ring-gray-400/50">
             <ul className="w-11/12 h-72 overflow-y-auto my-2">
               {airports.map((airport, i) => {
                 const name = airport.Name.replace(" Airport", "").replace(
