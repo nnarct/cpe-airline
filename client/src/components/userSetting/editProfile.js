1c71ad3ec05f4da8be83739840180e4e6b7503b9
nnarct <nannapat1551@gmail.com>
2023-05-06 01:11:24 +0700

    feat: connect to db, extract large component

--- client/src/components/userSetting/editProfile.js
+++ client/src/components/userSetting/editProfile.js
@@ -1,117 +1,150 @@
-import { useState } from "react";
+import Axios from "axios";
+import { useEffect, useRef, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { MyProfile } from "./myProfile";
+import { Row, ButtonWrap } from "./myProfileComps";
+import { Popup } from "../modal/popup";
+
 export const EditProfile = () => {
-  const user = {
-    FirstName: "Parichat",
-    LastName: "Sianainnnnnnnnnnnnnn",
-    Email: "Woonchat@gmail.com",
-    TelNo: "08-7296-2660" ,
-    password: "parichat"
-  };
+  const navigate = useNavigate();
   const [isEditing, setIsEditing] = useState(false);
+  const [isChecking, setIsChecking] = useState(false);
+  const { id } = useParams();
+  const edit = useRef(null);
+  const [user, setUser] = useState({
+    FirstName: "",
+    LastName: "",
+    Email: "",
+    TelNo: "",
+  });
+
+  useEffect(() => {
+    Axios.get("http://localhost:3001").then((res, err) => {
+      if (err) console.log(err);
+      if (res.data.Status !== "Success") navigate("/"); // Not logged in user
+    });
+    Axios.post("http://localhost:3001/showProfile", { id: id }).then(
+      (res, err) => {
+        if (err) console.log(err);
+        if (res.data.Status === "Success") setUser(res.data.Data);
+        else alert(res.data.Error);
+      }
+    );
+  }, []);
+
+  const handleSubmit = () => {
+    const form = edit.current;
+    const values = {
+      id: id,
+      FirstName: form["fn"].value,
+      LastName: form["ln"].value,
+      Email: form["em"].value,
+      TelNo: form["tel"].value,
+    };
+    Axios.post("http://localhost:3001/editProfile", values).then((res, err) => {
+      if (err) console.log(err);
+      if (res.data.Status === "Edit user successfully! :)") {
+        // alert(res.data.Status);
+        setUser(values);
+        setIsEditing(false);
+      } else alert(res.data.Error);
+    });
+  };
+
   return (
     <>
-      <div>
-        <h1 className="p-3 bg-white mb-4 text-3xl bg-blue-100 border-b border-neutral-200 font-bold">My Profile</h1>
-        {isEditing?<>
-          <div className="relative mb-4 flex flex-wrap items-stretch">
-          <span
-            className="flex items-center whitespace-nowrap rounded-l border border-r-0 
-            border-neutral-300 px-3 py-1 text-center leading-6 text-neutral-700 w-24 font-semibold"
-            >Firstname</span>
-          <input
-            type="text"
-            className="relative m-0 block w-px min-w-0 flex-auto rounded-r border border-neutral-300 
-            bg-white px-3 py-1 leading-6
-            text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] 
-            focus:border-primary focus:text-neutral-700 focus:ring ring-blue-200/40 
-            focus:outline-none"/>
-        </div>
+      <div className="container flex flex-col items-center w-full max-w-5xl">
+        {isEditing ? (
+          <>
+            <form ref={edit} className="w-full">
+              <Row head={"First Name"}>
+                <input
+                  name="fn"
+                  type="text"
+                  className="w-full rounded-r border border-neutral-300 bg-white px-3 py-1 outline-none focus:ring ring-blue-200/40"
+                  defaultValue={user.FirstName}
+                />
+              </Row>
 
-        <div className="relative mb-4 flex flex-wrap items-stretch">
-          <span
-            className="flex items-center whitespace-nowrap rounded-l border border-r-0 
-            border-neutral-300 px-3 py-1 text-center leading-6 text-neutral-700 w-24 font-semibold"
-            >Lastname</span>
-          <input
-            type="text"
-            className="relative m-0 block w-px min-w-0 flex-auto rounded-r border border-neutral-300 
-            bg-white px-3 py-1 leading-6
-            text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] 
-            focus:border-primary focus:text-neutral-700 focus:ring ring-blue-200/40 
-            focus:outline-none"/>
-        </div>
+              <Row head={"Last Name"}>
+                <input
+                  name="ln"
+                  type="text"
+                  className="w-full rounded-r border border-neutral-300 bg-white px-3 py-1 outline-none focus:ring ring-blue-200/40"
+                  defaultValue={user.LastName}
+                />
+              </Row>
 
+              <Row head={"Email"}>
+                <input
+                  name="em"
+                  type="email"
+                  className="w-full rounded-r border border-neutral-300 bg-white px-3 py-1 outline-none focus:ring ring-blue-200/40 "
+                  defaultValue={user.Email}
+                />
+              </Row>
 
-        <div className="relative mb-4 flex flex-wrap items-stretch">
-          <span
-            className="flex items-center whitespace-nowrap rounded-l border border-r-0 
-            border-neutral-300 px-3 py-1 text-center leading-6 text-neutral-700 w-24 font-semibold"
-            >Email</span>
-          <input
-            type="text"
-            className="relative m-0 block w-px min-w-0 flex-auto rounded-r border border-neutral-300 
-            bg-white px-3 py-1 leading-6
-            text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] 
-            focus:border-primary focus:text-neutral-700 focus:ring ring-blue-200/40 
-            focus:outline-none "/>
-        </div>
-        
-        <div className="relative mb-4 flex flex-wrap items-stretch">
-          <span
-            className="flex items-center whitespace-nowrap rounded-l border border-r-0 
-            border-neutral-300 px-3 py-1 text-center leading-6 text-neutral-700 w-24 font-semibold"
-            >TelNo</span>
-          <input
-            type="text"
-            className="relative m-0 block w-px min-w-0 flex-auto rounded-r border border-neutral-300 
-            bg-white px-3 py-1 leading-6
-            text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] 
-            focus:border-primary focus:text-neutral-700 focus:ring ring-blue-200/40 
-            focus:outline-none" />
-        </div>
+              <Row head={"Phone Number"}>
+                <input
+                  name="tel"
+                  type="text"
+                  className="w-full rounded-r border border-neutral-300 bg-white px-3 py-1 outline-none focus:ring ring-blue-200/40"
+                  defaultValue={user.TelNo}
+                />
+              </Row>
 
-        <div className="relative mb-4 flex flex-wrap items-stretch">
-          <span
-            className="flex items-center whitespace-nowrap rounded-l border border-r-0 
-            border-neutral-300 px-3 py-1 text-center leading-6 text-neutral-700 w-24 font-semibold"
-            >Password</span>
-          <input
-            type="text"
-            className="relative m-0 block w-px min-w-0 flex-auto rounded-r border border-neutral-300 
-            bg-white px-3 py-1 leading-6
-            text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] 
-            focus:border-primary focus:text-neutral-700 focus:ring ring-blue-200/40 
-            focus:outline-none "/>
-        </div>
-
-        <div className="w-full flex items-center justify-end">
-          <button onClick={() => setIsEditing(false)} className="bg-blue-500 hover:bg-blue-700 text-white 
-          font-bold py-2 px-6 rounded-full">
-           Save changes
-          </button>
-        </div></> : (<div className='flex flex-row gap-4'>
-          <div className="flex flex-col">
-            <div className="bg-white mb-2 bg-blue-100 border-b border-neutral-200 bg-red-10 p-2 m-2 rounded-lg font-semibold">Firstname</div>
-            <div className="bg-white mb-2 bg-blue-100 border-b border-neutral-200 bg-red-10 p-2 m-2 rounded-lg font-semibold">Lastname</div>
-            <div className="bg-white mb-2 bg-blue-100 border-b border-neutral-200 bg-red-10 p-2 m-2 rounded-lg font-semibold">Email</div>
-            <div className="bg-white mb-2 bg-blue-100 border-b border-neutral-200 bg-red-10 p-2 m-2 rounded-lg font-semibold">TelNo</div>
-            <div className="bg-white mb-2 bg-blue-100 border-b border-neutral-200 bg-red-10 p-2 m-2 rounded-lg font-semibold">Password</div>
-          </div>
-            <div className="flex flex-col w-1/2">
-            <div className="bg-white mb-2 bg-blue-100 border-b border-neutral-200 bg-red-10 p-2 m-2 rounded-lg font-thin">{user.FirstName}</div>
-            <div className="bg-white mb-2 bg-blue-100 border-b border-neutral-200 bg-red-10 p-2 m-2 rounded-lg font-thin">{user.LastName}</div>
-            <div className="bg-white mb-2 bg-blue-100 border-b border-neutral-200 bg-red-10 p-2 m-2 rounded-lg font-thin">{user.Email}</div>
-            <div className="bg-white mb-2 bg-blue-100 border-b border-neutral-200 bg-red-10 p-2 m-2 rounded-lg font-thin">{user.TelNo}</div>
-            <div className="bg-white mb-2 bg-blue-100 border-b border-neutral-200 bg-red-10 p-2 m-2 rounded-lg font-thin">{user.password}</div>
-          </div>
-
-          <div className="w-full flex flex-col items-center justify-end">
-          <button onClick={() => setIsEditing(true)} className="bg-blue-500 hover:bg-blue-700 text-white 
-          font-bold py-2 px-6 rounded-full">
-           Edit
-          </button>
-        </div></div>)}
-
+              <ButtonWrap>
+                <button
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setIsEditing(false);
+                  }}
+                  className="text-gray-500 rounded mx-3 font-semibold active:bg-gray-100 border border-gray-400 py-2 px-6 hover:ring ring-gray-300/60 bg-white"
+                >
+                  Cancle
+                </button>
+                <button
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setIsChecking(true);
+                  }}
+                  className="bg-green-500 text-white rounded mx-3 border border-green-500 font-semibold py-2 px-6 hover:ring ring-green-400/60 active:bg-green-600"
+                >
+                  Save changes
+                </button>
+              </ButtonWrap>
+              {isChecking ? (
+                <Popup>
+                  <div className="p-5 text-center rounded bg-white border shadow">
+                    <span className="text-2xl w-full">Are you sure?</span>
+                    <div className="flex space-x-3 pt-5">
+                      <button
+                        onClick={(e) => {
+                          e.preventDefault();
+                          setIsChecking(false);
+                        }}
+                        className="w-1/2 bg-white text-blue-500 border border-blue-500 py-1 px-2 rounded hover:ring ring-blue-200/40 focus:bg-blue-600"
+                      >
+                        Cancel
+                      </button>
+                      <button
+                        onClick={(e) => {
+                          e.preventDefault();
+                          handleSubmit();
+                        }}
+                        className="w-1/2 bg-blue-500 text-white border border-blue-500 py-1 px-2 rounded hover:ring ring-blue-200 focus:bg-blue-600"
+                      >
+                        Yes
+                      </button>
+                    </div>
+                  </div>
+                </Popup>
+              ) : null}
+            </form>
+          </>
+        ) : (
+          <MyProfile user={user} setIsEditing={setIsEditing} />
+        )}
       </div>
     </>
   );
