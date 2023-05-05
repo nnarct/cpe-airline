b2399ef604b5ad40e51880a4bc74e2bd534272ad
nnarct <nannapat1551@gmail.com>
2023-05-06 01:10:49 +0700

    style: remove css

--- client/src/components/userSetting/myFlight.js
+++ client/src/components/userSetting/myFlight.js
@@ -1,11 +1,7 @@
 export const MyFlight = () => {
   return (
     <>
-      <div className="w-full flex justify-center bg-green-100">
-        <div className="container">
-          My flight
-        </div>
-      </div>
+      
     </>
   );
 };
