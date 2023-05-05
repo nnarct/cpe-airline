2e0358ac53f9bcc2feb917dead87252496960f46
nnarct <nannapat1551@gmail.com>
2023-05-06 01:08:27 +0700

    feat: function to edit data in user table

---
+++ server/users/editProfile.js
@@ -0,0 +1,22 @@
+import { db } from "../index.js";
+
+export const editProfile = (req, res) => {
+  const sql =
+    "UPDATE user SET FirstName = ?, LastName = ?, Email = ?, TelNo = ? WHERE UserID =?";
+  db.query(
+    sql,
+    [
+      req.body.FirstName,
+      req.body.LastName,
+      req.body.Email,
+      req.body.TelNo,
+      req.body.id,
+    ],
+    (err, result) => {
+      if (err) {
+        return res.json({ Error: "Error while editing user..." });
+      }
+      return res.json({ Status: "Edit user successfully! :)" });
+    }
+  );
+}
