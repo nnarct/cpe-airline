dbe942d96e47e348e0db05fd7c0ce1819b51f236
nnarct <nannapat1551@gmail.com>
2023-05-06 01:10:30 +0700

    feat: beautify ui for showing user data

--- client/src/components/userSetting/myProfile.js
+++ client/src/components/userSetting/myProfile.js
@@ -1,23 +1,35 @@
-import { useState } from "react";
-import { useParams } from "react-router-dom";
-import { Navbar } from "../navbar";
-import { Sidebar } from "./sidebar/index.js";
-import { EditProfile } from "./editProfile.js";
-import { MyFlight } from "./myFlight.js";
-
-export const MyProfile = () => {
-  const { id } = useParams();
-  const [content, setContent] = useState("editProfile");
+import { ButtonWrap, Card, DataBlock, MyProfileWrapper, NameIcon } from "./myProfileComps";
+export const MyProfile = ({ user, setIsEditing }) => {
   return (
     <>
-      <Navbar />
-      <div className="max-w-screen min-h-calc flex">
-        <Sidebar content={content} setContent={setContent} />
-        <div className="bg-slate-50 w-full">
-          {content === "editProfile" && <EditProfile />}
-          {content === "myFlight" && <MyFlight />}
-        </div>
-      </div>
+      <MyProfileWrapper>
+        <Card className="flex items-center">
+          <NameIcon>{user.FirstName[0]}</NameIcon>
+          <div>
+            <DataBlock title={"First Name"} data={user.FirstName} />
+            <DataBlock title={"Last Name"} data={user.LastName} />
+          </div>
+        </Card>
+        <Card>
+          <DataBlock title={"Email"} data={user.Email} />
+        </Card>
+        <Card>
+          <DataBlock
+            title={"Phone Number"}
+            data={`${user.TelNo.substring(0, 3)}-${user.TelNo.substring(3, 6)}-${user.TelNo.substring(6, 19)}`}
+          />
+        </Card>
+
+        <ButtonWrap>
+          <button
+            onClick={() => setIsEditing(true)}
+            className="m-2 bg-blue-500 text-white 
+font-bold py-2 px-6 rounded hover:ring focus:bg-blue-600"
+          >
+            Edit
+          </button>
+        </ButtonWrap>
+      </MyProfileWrapper>
     </>
   );
 };
