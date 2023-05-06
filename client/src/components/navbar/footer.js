3f6c2ce57273eee3faba421aeab4329abbf3dcc2
nnarct <nannapat1551@gmail.com>
2023-05-06 23:33:49 +0700

    ref: footer for future dev

---
+++ client/src/components/navbar/footer.js
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return (
+    <>
+      <div className="flex justify-center bg-primary h-40 mt-12 text-white">
+        <div className="container p-3">{/* IMPLEMENT HERE */}</div>
+      </div>
+    </>
+  );
+};
