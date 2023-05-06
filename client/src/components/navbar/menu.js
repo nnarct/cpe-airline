a4752f1754de4e8cb8f6a16eb2673a59b060c8ae
nnarct <nannapat1551@gmail.com>
2023-05-06 22:57:34 +0700

    style: make transition faster

--- client/src/components/navbar/menu.js
+++ client/src/components/navbar/menu.js
@@ -4,7 +4,7 @@ export const Menu = ({ className, children }) => {
     <>
       <Link to="">
         <li
-          className={`h-12 flex items-center px-4 text-white hover:bg-black/30 transition duration-700 linear ${className}`}
+          className={`h-12 flex items-center px-4 text-white hover:bg-black/30 transition duration-500 linear ${className}`}
         >
           {children}
         </li>
