261bad911908a076e7a9029edc3c94e1c3e6ebb8
nnarct <nannapat1551@gmail.com>
2023-05-06 22:49:28 +0700

    ref: use chang password component

--- client/src/components/userSetting/myProfile.js
+++ client/src/components/userSetting/myProfile.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { ChangePassword } from "./changePassword";
 import {
   ButtonWrap,
   Card,
@@ -7,8 +7,6 @@ import {
   NameIcon,
 } from "./myProfileComps";
 export const MyProfile = ({ user, setIsEditing }) => {
-  const [newPass, setNewPass] = useState("");
-  const [newPass2, setNewPass2] = useState("");
   return (
     <>
       <MyProfileWrapper>
@@ -41,53 +39,7 @@ font-bold py-2 px-6 rounded hover:ring focus:bg-blue-600"
             Edit
           </button>
         </ButtonWrap>
-        <Card>
-          <DataBlock title={"Change Password"}>
-            <form action="">
-              <ul>
-                <li className="flex space-x-2 items-center">
-                  <p className="w-44 whitespace-nowrap w-1/6">
-                    Current Password
-                  </p>
-                  <input
-                    required
-                    type="password"
-                    className="rounded text-sm border w-4/6 hover:ring outline-none p-1 m-1 focus:ring ring-blue-200/40"
-                  />
-                </li>
-                <li className="flex space-x-2 items-center">
-                  <p className="w-44 whitespace-nowrap w-1/6">New Password</p>
-                  <input
-                    required
-                    type="password"
-                    className="rounded text-sm border w-4/6 hover:ring outline-none p-1 m-1 focus:ring ring-blue-200/40"
-                    onChange={(e) => {
-                      setNewPass(e.target.value);
-                    }}
-                  />
-                </li>
-                <li className="flex space-x-2 items-center">
-                  <p className="w-44 whitespace-nowrap w-1/6">
-                    Confirm New Password
-                  </p>
-                  <input
-                    required
-                    type="password"
-                    className="rounded text-sm border w-4/6 hover:ring outline-none p-1 m-1 focus:ring ring-blue-200/40"
-                    onChange={(e) => {
-                      setNewPass2(e.target.value);
-                    }}
-                  />
-                </li>
-                {newPass !== newPass2 ? (
-                  <div className="bg-red-200 p-1 rounded text-red-600 text-center">
-                    Password does not match.
-                  </div>
-                ) : null}
-              </ul>
-            </form>
-          </DataBlock>
-        </Card>
+        <ChangePassword />
       </MyProfileWrapper>
     </>
   );
