5d84838d9105934c8fbf121a30e1685f011028c1
Parichat Sien-in <Woonchat@gmail.com>
2023-05-07 18:13:16 +0700

    feat: finish component

--- client/src/components/homepage/bookingGroup/carrent/carrent.js
+++ client/src/components/homepage/bookingGroup/carrent/carrent.js
@@ -1,3 +1,42 @@
-export const Carrent = () => {
-  return <>Carrent</>;
+import Avisrental from "../../../../assets/carPartnerLogo/avisRental.png";
+import Kayakrental from "../../../../assets/carPartnerLogo/kayakRental.png";
+import Klookrental from "../../../../assets/carPartnerLogo/klookRental.png";
+import Rentalcarbangkok from "../../../../assets/carPartnerLogo/rentalCarbangkok.png";
+
+const CarPartner = ({ img, link }) => {
+  return (
+    <>
+      <div className="p-8">
+        <a href={link} target="_blank" rel="noreferrer">
+          <img alt="" src={img} className="object-contain h-40" />
+        </a>
+      </div>
+    </>
+  );
 };
+
+export const Carrent = () => {
+  return (
+    <>
+      <div className="flex justify-center">
+        <div className="max-w-[1600px] flex flex-col items-center">
+        <div className="text-center tracking-wider pb-10 py-10 text-1xl font-semibold uppercase text-primary">
+            Rent a car through our partners and enjoy your travelling.
+        </div>
+          <div className="flex flex-row justify-center">
+            <CarPartner img={Avisrental} link="https://www.avis.com/en/locations/th/bangkok" />
+            <CarPartner img={Kayakrental} link="https://www.kayak.com/Cheap-Bangkok-Car-Rentals.26166.cars.ksp" />
+            <CarPartner img={Klookrental} link="https://www.novotelbkk.com/th/" />
+            <CarPartner img={Rentalcarbangkok} link="https://www.klook.com/car-rentals/city/4-bangkok-car-rentals/" />
+          </div>
+        </div>
+
+        {/*
+        <div className="flex flex-col items-center ">
+          
+        </div> */}
+      </div>
+    </>
+  );
+};
+
--- client/src/components/homepage/bookingGroup/hotel/hotel.js
+++ client/src/components/homepage/bookingGroup/hotel/hotel.js
@@ -1,3 +1,41 @@
+import Centara from "../../../../assets/hotelPartnerLogo/centaraHotel.png";
+import Ibis from "../../../../assets/hotelPartnerLogo/ibisHotel.png";
+import Novotel from "../../../../assets/hotelPartnerLogo/novotelHotelandresort.png";
+import Sofitel from "../../../../assets/hotelPartnerLogo/sofitelHotelandresort.png";
+
+const HotelPartner = ({ img, link }) => {
+  return (
+    <>
+      <div className="p-8">
+        <a href={link} target="_blank" rel="noreferrer">
+          <img alt="" src={img} className="object-contain h-40" />
+        </a>
+      </div>
+    </>
+  );
+};
+
 export const Hotel = () => {
-  return <>Hotel</>;
+  return (
+    <>
+      <div className="flex justify-center">
+        <div className="max-w-[1600px] flex flex-col items-center">
+        <div className="text-center tracking-wider pb-10 py-10 text-1xl font-semibold uppercase text-primary">
+            Book a hotel room through our partners and enjoy your stay.
+        </div>
+          <div className="flex flex-row justify-center">
+            <HotelPartner img={Centara} link="https://www.centarahotelsresorts.com/th" />
+            <HotelPartner img={Ibis} link="https://all.accor.com/brands/ibis.th.shtml" />
+            <HotelPartner img={Novotel} link="https://www.novotelbkk.com/th/" />
+            <HotelPartner img={Sofitel} link="https://www.sofitelkrabiphokeethra.com/th/" />
+          </div>
+        </div>
+
+        {/*
+        <div className="flex flex-col items-center ">
+          
+        </div> */}
+      </div>
+    </>
+  );
 };
