aa528f5e7b6622a52550a1743848bc8014e7cf5a
nnarct <nannapat1551@gmail.com>
2023-05-11 20:23:17 +0700

    feat: add change day button

--- client/src/components/searchResult/searchResult.js
+++ client/src/components/searchResult/searchResult.js
@@ -1,11 +1,12 @@
 import Axios from "axios";
 import moment from "moment";
 import { useEffect, useState } from "react";
-import { useLocation, Link } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { formatDate } from "./function";
 import { FlightDetail } from "./flightdetail";
 
 export const SearchResult = () => {
+  const navigate = useNavigate();
   const location = useLocation();
   const params = new URLSearchParams(location.search);
   const v = {
@@ -35,7 +36,6 @@ export const SearchResult = () => {
       (res, err) => {
         if (err) console.log(err);
         if (res.data.Status === "Success") {
-          console.log(res.data.Flights);
           setFlights(res.data.Flights);
         } else {
           console.log(res.data.Error);
@@ -58,6 +58,62 @@ export const SearchResult = () => {
     }
     return "unknown airport";
   };
+  const prevDay = (e) => {
+    e.preventDefault();
+    const date = new Date(v.departure);
+    date.setDate(date.getDate() - 1);
+    const newDate = moment(date).format("YYYY-MM-DD");
+    navigate(
+      "/search?from=" +
+        v.from +
+        "&to=" +
+        v.to +
+        "&departure=" +
+        newDate +
+        "&arrival=" +
+        v.arrival +
+        "&adult=" +
+        v.adult +
+        "&child=" +
+        v.child +
+        "&infant=" +
+        v.infant +
+        "&class=" +
+        v.class +
+        "&isReturn=" +
+        v.isReturn +
+        ""
+    );
+    window.location.reload();
+  };
+  const nextDay = (e) => {
+    e.preventDefault();
+    const date = new Date(v.departure);
+    date.setDate(date.getDate() + 1);
+    const newDate = moment(date).format("YYYY-MM-DD");
+    navigate(
+      "/search?from=" +
+        v.from +
+        "&to=" +
+        v.to +
+        "&departure=" +
+        newDate +
+        "&arrival=" +
+        v.arrival +
+        "&adult=" +
+        v.adult +
+        "&child=" +
+        v.child +
+        "&infant=" +
+        v.infant +
+        "&class=" +
+        v.class +
+        "&isReturn=" +
+        v.isReturn +
+        ""
+    );
+    window.location.reload();
+  };
   return (
     <>
       <div className="bg-gray-100 min-h-screen w-screen flex flex-col items-center">
@@ -75,6 +131,20 @@ export const SearchResult = () => {
           <span className="px-1 bg-gray-200 rounded">
             {formatDate(v.departure)}
           </span>
+        </div>
+        <div>
+          <button
+            onClick={prevDay}
+            className="bg-blue-300 hover:ring border rounded mx-3 px-3"
+          >
+            Previous Day
+          </button>
+          <button
+            onClick={nextDay}
+            className="bg-blue-300 hover:ring border rounded mx-3 px-3"
+          >
+            Next Day
+          </button>
         </div>
         {Flights &&
           Flights.map((flight, i) => {
