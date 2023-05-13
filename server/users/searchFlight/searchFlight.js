b8d63fb8904fd4cc9e7fb6aa65f614bfbe835be8
Nannapat Intharasing <nannapat1551@gmail.com>
2023-05-14 02:05:47 +0700

    fix: remove trash

--- server/users/searchFlight/searchFlight.js
+++ server/users/searchFlight/searchFlight.js
@@ -1,16 +1,14 @@
 import { db } from "../../index.js";
 
-export const SearchFlights = (req, res) => {
+export const searchFlights = (req, res) => {
   const sql =
     "SELECT * FROM flight WHERE OriginAirportID = ? AND DestinationAirportID = ? AND DepartureTime LIKE ?;";
   db.query(
     sql,
     [req.body.from, req.body.to, req.body.departure + "%"],
     (err, response) => {
-      if (err) {
-        console.log(err);
+      if (err)
         return res.json({ Error: "Error while searching flight in db..." });
-      }
       if (response.length > 0)
         return res.json({ Status: "Success", Flights: response });
       return res.json({ Error: "No flight found." });
