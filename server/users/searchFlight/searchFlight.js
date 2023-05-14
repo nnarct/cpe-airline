e0338388304b7d5d474519d1237343f85e69b21b
Nannapat Intharasing <nannapat1551@gmail.com>
2023-05-14 17:34:57 +0700

    feat: use inner join

--- server/users/searchFlight/searchFlight.js
+++ server/users/searchFlight/searchFlight.js
@@ -2,7 +2,7 @@ import { db } from "../../index.js";
 
 export const searchFlights = (req, res) => {
   const sql =
-    "SELECT * FROM flight WHERE OriginAirportID = ? AND DestinationAirportID = ? AND DepartureTime LIKE ?;";
+    "SELECT f.FlightID, f.FlightNumber, f.AirlineID, al.Name as AirlineName, f.DepartureTime, f.ArrivalTime, oa.Name AS Origin, oa.IATA as OriIATA, da.Name AS Destination, da.IATA as DesIATA, f.PlaneID FROM flight AS f INNER JOIN airport AS oa ON oa.AirportID = f.OriginAirportID INNER JOIN airport AS da ON da.AirportID = f.DestinationAirportID INNER JOIN airline AS al ON al.AirlineID = f.AirlineID WHERE f.OriginAirportID = ? AND f.DestinationAirportID = ? AND f.DepartureTime LIKE ?;";
   db.query(
     sql,
     [req.body.from, req.body.to, req.body.departure + "%"],
