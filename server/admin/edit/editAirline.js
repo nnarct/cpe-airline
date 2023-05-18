5b39a3a44a51b6ad820c91847346a8d8d5156ec2
Nannapat Intharasing <nannapat1551@gmail.com>
2023-05-18 23:18:02 +0700

    ref: format file

--- server/admin/edit/editAirline.js
+++ server/admin/edit/editAirline.js
@@ -1,15 +1,10 @@
 import { db } from "../../index.js";
 export const editAirline = (req, res) => {
-  const sql =
-    "UPDATE airline SET Name = ?, Link = ? WHERE AirlineID =?";
-  db.query(
-    sql,
-    [req.body.Name, req.body.Link, req.body.id],
-    (err, result) => {
-      if (err) {
-        return res.json({ Error: "Error while editing airline..." });
-      }
-      return res.json({ Status: "Edit airline successfully! :)" });
+  const sql = "UPDATE airline SET Name = ?, Link = ? WHERE AirlineID =?";
+  db.query(sql, [req.body.Name, req.body.Link, req.body.id], (err, result) => {
+    if (err) {
+      return res.json({ Error: "Error while editing airline..." });
     }
-  );
+    return res.json({ Status: "Edit airline successfully! :)" });
+  });
 };
