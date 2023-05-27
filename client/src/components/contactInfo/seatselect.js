42d67680dbb9b18d3b7a47d9fe610e5f25de9e50
Nannapat Intharasing <nannapat1551@gmail.com>
2023-05-27 22:35:46 +0700

    feat: fix misspelled word

--- client/src/components/contactInfo/seatselect.js
+++ client/src/components/contactInfo/seatselect.js
@@ -1,149 +1,142 @@
-import { TbPointFilled } from "react-icons/tb"
-import { Seat } from "./seat"
+import { TbPointFilled } from "react-icons/tb";
+import { Seat } from "./seat";
 
 //  0 : avaliable
 //  1 : not valiable
 const seatsRowA = [
-    {
-        seatNumber: "A1",
-        seatStatus: 1,
-    },
-    {
-        seatNumber: "A2",
-        seatStatus: 1,
-    },
-    {
-        seatNumber: "A3",
-        seatStatus: 0,
-    },
+  {
+    seatNumber: "A1",
+    seatStatus: 1,
+  },
+  {
+    seatNumber: "A2",
+    seatStatus: 1,
+  },
+  {
+    seatNumber: "A3",
+    seatStatus: 0,
+  },
 ];
 const seatsRowB = [
-    {
-        seatNumber: "B1",
-        seatStatus: 1,
-    },
-    {
-        seatNumber: "B2",
-        seatStatus: 1,
-    },
-    {
-        seatNumber: "B3",
-        seatStatus: 0,
-    },
+  {
+    seatNumber: "B1",
+    seatStatus: 1,
+  },
+  {
+    seatNumber: "B2",
+    seatStatus: 1,
+  },
+  {
+    seatNumber: "B3",
+    seatStatus: 0,
+  },
 ];
 const seatsRowC = [
-    {
-        seatNumber: "C1",
-        seatStatus: 1,
-    },
-    {
-        seatNumber: "C2",
-        seatStatus: 1,
-    },
-    {
-        seatNumber: "C3",
-        seatStatus: 0,
-    },
+  {
+    seatNumber: "C1",
+    seatStatus: 1,
+  },
+  {
+    seatNumber: "C2",
+    seatStatus: 1,
+  },
+  {
+    seatNumber: "C3",
+    seatStatus: 0,
+  },
 ];
 const seatsRowH = [
-    {
-        seatNumber: "H1",
-        seatStatus: 1,
-    },
-    {
-        seatNumber: "H2",
-        seatStatus: 1,
-    },
-    {
-        seatNumber: "H3",
-        seatStatus: 0,
-    },
+  {
+    seatNumber: "H1",
+    seatStatus: 1,
+  },
+  {
+    seatNumber: "H2",
+    seatStatus: 1,
+  },
+  {
+    seatNumber: "H3",
+    seatStatus: 0,
+  },
 ];
 const seatsRowJ = [
-    {
-        seatNumber: "J1",
-        seatStatus: 1,
-    },
-    {
-        seatNumber: "J2",
-        seatStatus: 1,
-    },
-    {
-        seatNumber: "J3",
-        seatStatus: 0,
-    },
+  {
+    seatNumber: "J1",
+    seatStatus: 1,
+  },
+  {
+    seatNumber: "J2",
+    seatStatus: 1,
+  },
+  {
+    seatNumber: "J3",
+    seatStatus: 0,
+  },
 ];
 const seatsRowK = [
-    {
-        seatNumber: "K1",
-        seatStatus: 1,
-    },
-    {
-        seatNumber: "K2",
-        seatStatus: 1,
-    },
-    {
-        seatNumber: "K3",
-        seatStatus: 0,
-    },
+  {
+    seatNumber: "K1",
+    seatStatus: 1,
+  },
+  {
+    seatNumber: "K2",
+    seatStatus: 1,
+  },
+  {
+    seatNumber: "K3",
+    seatStatus: 0,
+  },
 ];
 
 export const Seatselect = () => {
-    return (
+  return (
+    <div>
+      <h1 className="text-2xl  bg-white font-bold text-primary border-b border-primary/20 mb-1 pb-1">
+        Seat Number
+      </h1>
+      <div className="flex">
+        <p className="flex items-center text-m">
+          <TbPointFilled color="orange" size="2.5em" border="solid black" />
+          avaliable
+        </p>
+        <p className="flex items-center text-m">
+          <TbPointFilled color="gray" size="2.5em" />
+          not avaliable
+        </p>
+      </div>
+      <div className="flex ">
         <div>
-            <h1 className="text-2xl  bg-white font-bold text-primary border-b border-primary/20 mb-1 pb-1">Seat Number</h1>
-            <diV className="flex">
-                <p className="flex items-center text-m"><TbPointFilled color="orange" size="2.5em" border="solid black" />avaliable</p>
-                <p className="flex items-center text-m"><TbPointFilled color="gray" size="2.5em" />not avaliable</p>
-            </diV>
-            <div className="flex ">
-                <diV>
-                    {seatsRowA.map((seat) => {
-                        return (
-                            <Seat {...seat} />
-                        );
-                    })}
-                </diV>
-                <diV>
-                    {seatsRowB.map((seat) => {
-                        return (
-                            <Seat {...seat} />
-                        );
-                    })}
-                </diV>
-                <diV>
-                    {seatsRowC.map((seat) => {
-                        return (
-                            <Seat {...seat} />
-                        );
-                    })}
-                </diV>
-
-                <diV className="ml-8">
-                    {seatsRowH.map((seat) => {
-                        return (
-                            <Seat {...seat} />
-                        );
-                    })}
-                </diV>
-                <diV>
-                    {seatsRowJ.map((seat) => {
-                        return (
-                            <Seat {...seat} />
-                        );
-                    })}
-                </diV>
-                <diV>
-                    {seatsRowK.map((seat) => {
-                        return (
-                            <Seat {...seat} />
-                        );
-                    })}
-                </diV>
-
-            </div>
-
+          {seatsRowA.map((seat, i) => {
+            return <Seat {...seat} key={i} />;
+          })}
+        </div>
+        <div>
+          {seatsRowB.map((seat, i) => {
+            return <Seat {...seat} key={i} />;
+          })}
+        </div>
+        <div>
+          {seatsRowC.map((seat, i) => {
+            return <Seat {...seat} key={i} />;
+          })}
         </div>
 
-    );
+        <div className="ml-8">
+          {seatsRowH.map((seat, i) => {
+            return <Seat {...seat} key={i} />;
+          })}
+        </div>
+        <div>
+          {seatsRowJ.map((seat, i) => {
+            return <Seat {...seat} key={i} />;
+          })}
+        </div>
+        <div>
+          {seatsRowK.map((seat, i) => {
+            return <Seat {...seat} key={i} />;
+          })}
+        </div>
+      </div>
+    </div>
+  );
 };
