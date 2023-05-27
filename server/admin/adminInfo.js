d443c09ac13efa51f14e6b590d325b10612a3983
Nannapat Intharasing <nannapat1551@gmail.com>
2023-05-28 02:16:44 +0700

    fix: change the way info is retrieved

--- server/admin/adminInfo.js
+++ server/admin/adminInfo.js
@@ -3,6 +3,9 @@ import jwt from "jsonwebtoken";
 export const adminInfo = (req, res) => {
   let airlines = [];
   const sql = "SELECT * FROM airline";
+  const sql2 =
+    "SELECT FirstName, LastName, Email, TelNo, Username, EmployeeID FROM employee WHERE EmployeeID = ?";
+  let id;
   db.query(sql, (err, result) => {
     if (err) return res.json({ Error: "Error while getting airlines..." });
     else if (result.length > 0) {
@@ -13,10 +16,17 @@ export const adminInfo = (req, res) => {
     jwt.verify(req.body.cookieID, "admin-secret-key", (err, decoded) => {
       if (err) return res.json({ Error: "Admin token is not ok" });
       delete decoded.info.Password;
-      return res.json({
-        Status: "Success",
-        Info: decoded.info,
-        Airlines: airlines,
+      id = decoded.info.EmployeeID;
+      db.query(sql2, [id], (err, data) => {
+        if (err)
+          return res.json({ Error: "Error while getting admin info..." });
+        else if (data.length > 0) {
+          return res.json({
+            Status: "Get admin info successfully! :)",
+            Info: data[0],
+            Airlines: airlines,
+          });
+        } else return res.json({ Error: "No admin found..." });
       });
     });
   } else return res.json({ Error: "You are not authenticated admin" });
