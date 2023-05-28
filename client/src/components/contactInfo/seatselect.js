1b869cf0697901f3290605079b6f8a54f4974f5d
Nannapat Intharasing <nannapat1551@gmail.com>
2023-05-28 13:16:57 +0700

    feat: retrieve seat plan from db and display

--- client/src/components/contactInfo/seatselect.js
+++ client/src/components/contactInfo/seatselect.js
@@ -1,142 +1,81 @@
+import Axios from "axios";
 import { TbPointFilled } from "react-icons/tb";
 import { Seat } from "./seat";
+import { useEffect, useState } from "react";
 
-//  0 : avaliable
-//  1 : not valiable
-const seatsRowA = [
-  {
-    seatNumber: "A1",
-    seatStatus: 1,
-  },
-  {
-    seatNumber: "A2",
-    seatStatus: 1,
-  },
-  {
-    seatNumber: "A3",
-    seatStatus: 0,
-  },
-];
-const seatsRowB = [
-  {
-    seatNumber: "B1",
-    seatStatus: 1,
-  },
-  {
-    seatNumber: "B2",
-    seatStatus: 1,
-  },
-  {
-    seatNumber: "B3",
-    seatStatus: 0,
-  },
-];
-const seatsRowC = [
-  {
-    seatNumber: "C1",
-    seatStatus: 1,
-  },
-  {
-    seatNumber: "C2",
-    seatStatus: 1,
-  },
-  {
-    seatNumber: "C3",
-    seatStatus: 0,
-  },
-];
-const seatsRowH = [
-  {
-    seatNumber: "H1",
-    seatStatus: 1,
-  },
-  {
-    seatNumber: "H2",
-    seatStatus: 1,
-  },
-  {
-    seatNumber: "H3",
-    seatStatus: 0,
-  },
-];
-const seatsRowJ = [
-  {
-    seatNumber: "J1",
-    seatStatus: 1,
-  },
-  {
-    seatNumber: "J2",
-    seatStatus: 1,
-  },
-  {
-    seatNumber: "J3",
-    seatStatus: 0,
-  },
-];
-const seatsRowK = [
-  {
-    seatNumber: "K1",
-    seatStatus: 1,
-  },
-  {
-    seatNumber: "K2",
-    seatStatus: 1,
-  },
-  {
-    seatNumber: "K3",
-    seatStatus: 0,
-  },
-];
 
-export const Seatselect = () => {
+export const Seatselect = ({ params }) => {
+  const [availableDepartureSeat, setAvailableDepartureSeat] = useState([]);
+  const [availableReturnSeat, setAvailableReturnSeat] = useState([]);
+  const [bookedDepartureSeat, setBookedDepartureSeat] = useState([]);
+  const [bookedReturnSeat, setBookedReturnSeat] = useState([]);
+  useEffect(() => {
+    Axios.post("http://localhost:3001/getAvailableSeat", {
+      isReturn: params.get("isReturn"),
+      departureFlightID: params.get("departureFlightID"),
+      returnFlightID: params.get("returnFlightID"),
+      class: params.get("class"),
+    }).then((res, err) => {
+      if (err) console.log(err);
+      if (res.data.Status === "Success") {
+        setAvailableDepartureSeat(res.data.availableDepartureSeat);
+        setBookedDepartureSeat(res.data.bookedDepartureSeat);
+
+        if (params.get("isReturn") === 1) {
+          setAvailableReturnSeat(res.data.ReturnSeat);
+          setBookedReturnSeat(res.data.bookedReturnSeat);
+        }
+      } else console.log(res.data.Error);
+    });
+    
+  }, []);
+
+  const combinedSeats = [...availableDepartureSeat].map((seat) => {
+    return {
+      ...seat,
+      isBooked: bookedDepartureSeat.some(
+        (bookedSeat) => bookedSeat.SeatID === seat.SeatID
+      ),
+    };
+  });
+  const rows = combinedSeats.reduce((acc, seat) => {
+    if (seat.SeatCode.includes("A")) {
+      acc.push([seat]);
+    } else {
+      const lastRow = acc[acc.length - 1];
+      lastRow.push(seat);
+    }
+    return acc;
+  }, []);
+
   return (
-    <div>
-      <h1 className="text-2xl  bg-white font-bold text-primary border-b border-primary/20 mb-1 pb-1">
-        Seat Number
-      </h1>
-      <div className="flex">
-        <p className="flex items-center text-m">
-          <TbPointFilled color="orange" size="2.5em" border="solid black" />
-          avaliable
-        </p>
-        <p className="flex items-center text-m">
-          <TbPointFilled color="gray" size="2.5em" />
-          not avaliable
-        </p>
-      </div>
-      <div className="flex ">
-        <div>
-          {seatsRowA.map((seat, i) => {
-            return <Seat {...seat} key={i} />;
-          })}
-        </div>
-        <div>
-          {seatsRowB.map((seat, i) => {
-            return <Seat {...seat} key={i} />;
-          })}
-        </div>
-        <div>
-          {seatsRowC.map((seat, i) => {
-            return <Seat {...seat} key={i} />;
-          })}
-        </div>
-
-        <div className="ml-8">
-          {seatsRowH.map((seat, i) => {
-            return <Seat {...seat} key={i} />;
-          })}
-        </div>
-        <div>
-          {seatsRowJ.map((seat, i) => {
-            return <Seat {...seat} key={i} />;
-          })}
-        </div>
-        <div>
-          {seatsRowK.map((seat, i) => {
-            return <Seat {...seat} key={i} />;
+    <>
+      <div className="w-full max-w-1000 mx-auto py-5 px-2 flex space-y-3 space-y-reverse sm:space-y-0 sm:space-x-3 flex-col-reverse sm:flex-row">
+        <div className="w-full bg-white border border-primary/40 rounded-md py-3 px-5">
+          <h1 className="text-2xl  bg-white font-bold text-primary border-b border-primary/20 mb-1 pb-1">
+            Seat Number
+          </h1>
+          <div className="flex">
+            <p className="flex items-center text-m">
+              <TbPointFilled color="orange" size="2.5em" border="solid black" />
+              avaliable
+            </p>
+            <p className="flex items-center text-m">
+              <TbPointFilled color="gray" size="2.5em" />
+              not avaliable
+            </p>
+          </div>
+          {rows.map((row, index) => {
+            return (
+              <div className="grid grid-cols-13 gap-4 my-4" key={index}>
+                {row.map((seat, seatIndex) => {
+                  return <Seat key={seatIndex} seat={seat} />;
+                })}
+              </div>
+            );
           })}
         </div>
       </div>
-    </div>
+    </>
   );
 };
