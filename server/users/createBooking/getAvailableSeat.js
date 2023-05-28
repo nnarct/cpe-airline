96f13c45e9880ab5cde6475c87c2f8d9e75ff148
Nannapat Intharasing <nannapat1551@gmail.com>
2023-05-28 18:48:27 +0700

    fix: add order by seat length to sql

--- server/users/createBooking/getAvailableSeat.js
+++ server/users/createBooking/getAvailableSeat.js
@@ -2,10 +2,10 @@ import e from "express";
 import { db } from "../../index.js";
 
 export const getAvailableSeat = (req, res) => {
-  const sql = `SELECT s.*, c.Name AS Class FROM seat s INNER JOIN class c ON c.ClassID = s.ClassID INNER JOIN plane pl ON pl.PlaneID = c.PlaneID LEFT JOIN passenger p ON p.SeatID = s.SeatID LEFT JOIN booking b ON b.BookingID = p.BookingID AND b.FlightID = ? WHERE pl.AirlineID = (  SELECT AirlineID FROM flight WHERE FlightID = ? ) AND b.BookingID IS NULL ORDER BY s.SeatCode `;
+  const sql = `SELECT s.*, c.Name AS Class FROM seat s INNER JOIN class c ON c.ClassID = s.ClassID INNER JOIN plane pl ON pl.PlaneID = c.PlaneID LEFT JOIN passenger p ON p.SeatID = s.SeatID LEFT JOIN booking b ON b.BookingID = p.BookingID AND b.FlightID = ? WHERE pl.AirlineID = (  SELECT AirlineID FROM flight WHERE FlightID = ? ) AND b.BookingID IS NULL ORDER BY LENGTH(s.SeatCode),s.SeatCode `;
   const sqlBooked = `SELECT s.* FROM seat s INNER JOIN class c ON c.ClassID = s.ClassID INNER JOIN plane pl ON pl.PlaneID = c.PlaneID INNER JOIN passenger p ON p.SeatID = s.SeatID INNER JOIN booking b ON b.BookingID = p.BookingID WHERE pl.AirlineID = ( SELECT AirlineID FROM flight WHERE FlightID = ? ) AND b.FlightID = ?;`;
   const sqlAllSeat =
-    "SELECT s.*, c.Name AS Class FROM seat s INNER JOIN class c ON c.ClassID = s.ClassID INNER JOIN plane pl ON pl.PlaneID = c.PlaneID INNER JOIN flight f ON f.PlaneID = pl.PlaneID AND f.FlightID = ? WHERE pl.AirlineID = (SELECT AirlineID FROM flight WHERE FlightID = ?) ORDER BY s.SeatCode";
+    "SELECT s.*, c.Name AS Class FROM seat s INNER JOIN class c ON c.ClassID = s.ClassID INNER JOIN plane pl ON pl.PlaneID = c.PlaneID INNER JOIN flight f ON f.PlaneID = pl.PlaneID AND f.FlightID = ? WHERE pl.AirlineID = (SELECT AirlineID FROM flight WHERE FlightID = ?) ORDER BY LENGTH(s.SeatCode),s.SeatCode";
   let availableDepartureSeat = [];
   let availableReturnSeat = [];
   let bookedDepartureSeat = [];
