8726b80de8ee8921d62f05572cd49e016ad112f9
Nannapat Intharasing <nannapat1551@gmail.com>
2023-05-28 11:56:53 +0700

    ref: relines group by function

--- server/index.js
+++ server/index.js
@@ -35,6 +35,9 @@ import { editEmployee } from "./admin/edit/editEmployee.js";
 import { editAirline } from "./admin/edit/editAirline.js";
 import { editUser } from "./admin/edit/editUser.js";
 import { editPassenger } from "./admin/edit/editPassenger.js";
+import { editAirport } from "./admin/edit/editAirport.js";
+import { editFlight } from "./admin/edit/editFlight.js";
+import { editPlane } from "./admin/edit/editPlane.js";
 
 // insert
 import { insertAirport } from "./admin/insert/insertAirport.js";
@@ -50,41 +53,49 @@ import { deletePassenger } from "./admin/delete/deletePassenger.js";
 import { deleteAirport } from "./admin/delete/deleteAirport.js";
 import { deletePlane } from "./admin/delete/deletePlane.js";
 import { deleteFlight } from "./admin/delete/deleteFlight.js";
-
+import { deleteUser } from "./admin/delete/deleteUser.js";
 
 // verify auth
 import { verifyUserName, getUserName } from "./users/getUserName.js";
 import { airportListUser } from "./users/airportList.js";
+
+// user my profile
 import { showProfile } from "./users/showProfile.js";
 import { editProfile } from "./users/editProfile.js";
 import { changePassword } from "./users/changePassword.js";
-import { AmadeusSearchFlights } from "./users/amadeus.js";
+import { getUserBooking } from "./users/myFlight/getUserBooking.js";
+
+// search flight
 import { searchFlights } from "./users/searchFlight/searchFlight.js";
-import { getFlight } from "./users/searchFlight/getFlight.js";
-import { flightInfo } from "./users/searchFlight/flightInfo.js";
-import { insertBooking } from "./users/searchFlight/insertBooking.js";
-import { getPayment } from "./users/searchFlight/getPayment.js";
-import { getInvoice } from "./users/searchFlight/getInvoice.js";
-import { editAirport } from "./admin/edit/editAirport.js";
-import { editFlight } from "./admin/edit/editFlight.js";
+
+// create booking
 import {
   verifyOwner,
   verifyOwnerRes,
 } from "./users/searchFlight/verifyowner.js";
-import { editPlane } from "./admin/edit/editPlane.js";
-import { genderCount } from "./admin/dashboard/genderCount.js"
-import { bookingCount } from "./admin/dashboard/bookCount.js"
-import { getFlightCountsBySection } from "./admin/dashboard/Destination.js"
-import { getUserBooking } from "./users/myFlight/getUserBooking.js";
-import { deleteUser } from "./admin/delete/deleteUser.js";
+import { getFlight } from "./users/createBooking/getFlight.js";
+import { flightInfo } from "./users/createBooking/flightInfo.js";
+import { insertBooking } from "./users/createBooking/insertBooking.js";
+import { getPayment } from "./users/createBooking/getPayment.js";
+import { getInvoice } from "./users/createBooking/getInvoice.js";
+import { addOnInfo, getBase } from "./users/createBooking/addOns.js";
+import { getAvailableSeat } from "./users/createBooking/getAvailableSeat.js";
+
 //import { deleteFlight } from "../client/delete/deleteFlight.js";
 import { selectFlight } from "./admin/restrict/flight/select.js";
-import { addOnInfo, getBase } from "./users/searchFlight/addOns.js";
+
+
+// admin my profile
 import { adminInfo } from "./admin/adminInfo.js";
 import { editSystemProfile } from "./admin/edit/editSystemProfile.js";
 import { editAdminProfile } from "./admin/edit/editAdminProfile.js";
+
+// dashboard
+import { bookingCount } from "./admin/dashboard/bookCount.js"
 import { flightCount } from "./admin/dashboard/flightCount.js";
 import { userCount } from "./admin/dashboard/userCount.js";
+import { genderCount } from "./admin/dashboard/genderCount.js"
+import { getFlightCountsBySection } from "./admin/dashboard/Destination.js"
 
 const app = express();
 app.use(express.json());
@@ -177,6 +188,7 @@ app.post("/getPayment", getPayment);
 app.post("/getInvoice", getInvoice);
 app.get("/invoice/userauth", verifyOwner, verifyOwnerRes);
 app.post("/getUserBooking", getUserBooking);
+app.post("/getAvailableSeat", getAvailableSeat);
 
 
 // Dashboard
