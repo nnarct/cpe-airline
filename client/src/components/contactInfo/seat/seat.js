4c50035340388563c840736a304715812a3be58f
Nannapat Intharasing <nannapat1551@gmail.com>
2023-05-28 17:09:54 +0700

    feat: select seat for each passenger and flight

--- client/src/components/contactInfo/seat/seat.js
+++ client/src/components/contactInfo/seat/seat.js
@@ -1,11 +1,36 @@
-export const Seat = ({ seat }) => {
+import Swal from "sweetalert2";
+
+export const checkUniqueData = (dep, ret) => {
+  const combined = [...dep, ...ret];
+  const uniqueSet = new Set(combined);
+  return combined.length === uniqueSet.size;
+};
+export const Seat = ({ id, seat, seatID, setSeatID, dif }) => {
+  const handleSeat = (index, element) => {
+    if(seatID.includes(element)) return; // seat is already selected
+    const newArray = seatID.map((seat, i) => {
+      if (i === index) return element;
+      return seat;
+    });
+    setSeatID(newArray);
+  };
+
   if (seat.isBooked)
     return (
       <>
         <div
-          className={`col-span-2 flex items-center justify-center font-bold text-xl w-full text-gray-500 border-2 border-gray-600 py-4 bg-gray-300 rounded-lg ${
+          className={`col-span-2 flex items-center justify-center font-bold text-xl w-full text-gray-500 border-2 border-gray-400 py-4 bg-gray-300 rounded-lg ${
             seat.SeatCode.includes("C") ? "mr-2" : ""
-          }`}
+          }  `}
+          onClick={() => {
+            Swal.fire({
+              position: "top-end",
+              html: `<h1 class="text-2xl text-red-600">Seat ${seat.SeatCode} is not available.</h1>`,
+              showConfirmButton: false,
+              timer: 1000,
+              backdrop: false,
+            });
+          }}
         >
           {seat.SeatCode}
         </div>
@@ -15,11 +40,12 @@ export const Seat = ({ seat }) => {
   return (
     <>
       <div
-        className={`col-span-2 shadow flex items-center justify-center w-full cursor-pointer font-bold text-xl border-2 py-4 border-gray-600 rounded-lg ${
-          seat.Class === "Economy"
-            ? "bg-blue-300 border-blue-100 hover:border-blue-400"
-            : "bg-green-300 border-green-100 hover:border-green-400"
-        }  `}
+        className={`col-span-2 shadow flex items-center justify-center w-full cursor-pointer font-bold text-xl border-2 py-4 rounded-lg  ${seatID.includes(seat.SeatID) ? "bg-red-400" : 
+        (  seat.Class === "Economy"
+          ? "bg-blue-300 border-blue-100 hover:border-blue-400"
+          : "bg-green-300 border-green-100 hover:border-green-400")
+        } `}
+        onClick={() => handleSeat(id, seat.SeatID)}
       >
         {seat.SeatCode}
       </div>
--- client/src/components/contactInfo/seat/seatSelect.js
+++ client/src/components/contactInfo/seat/seatSelect.js
@@ -1,10 +1,15 @@
 import Axios from "axios";
 import { TbPointFilled } from "react-icons/tb";
-import { Seat } from "./seat";
+import { Seat, checkUniqueData } from "./seat";
 import { useEffect, useState } from "react";
 
-
-export const SeatSelect = ({ params }) => {
+export const SeatSelect = ({
+  params,
+  depSeats,
+  setDepSeats,
+  retSeats,
+  setRetSeats,
+}) => {
   const [availableDepartureSeat, setAvailableDepartureSeat] = useState([]);
   const [availableReturnSeat, setAvailableReturnSeat] = useState([]);
   const [bookedDepartureSeat, setBookedDepartureSeat] = useState([]);
@@ -20,17 +25,15 @@ export const SeatSelect = ({ params }) => {
       if (res.data.Status === "Success") {
         setAvailableDepartureSeat(res.data.availableDepartureSeat);
         setBookedDepartureSeat(res.data.bookedDepartureSeat);
-
-        if (params.get("isReturn") === 1) {
-          setAvailableReturnSeat(res.data.ReturnSeat);
+        if (params.get("isReturn") === '1' || params.get("isReturn") === 1) {
+          setAvailableReturnSeat(res.data.availableReturnSeat);
           setBookedReturnSeat(res.data.bookedReturnSeat);
         }
-      } else console.log(res.data.Error);
+      } 
     });
-    
   }, []);
 
-  const combinedSeats = [...availableDepartureSeat].map((seat) => {
+  const combinedDepSeats = [...availableDepartureSeat].map((seat) => {
     return {
       ...seat,
       isBooked: bookedDepartureSeat.some(
@@ -38,7 +41,7 @@ export const SeatSelect = ({ params }) => {
       ),
     };
   });
-  const rows = combinedSeats.reduce((acc, seat) => {
+  const rowsDep = combinedDepSeats.reduce((acc, seat) => {
     if (seat.SeatCode.includes("A")) {
       acc.push([seat]);
     } else {
@@ -47,7 +50,25 @@ export const SeatSelect = ({ params }) => {
     }
     return acc;
   }, []);
-
+  const combinedRetSeats = [...availableReturnSeat].map((seat) => {
+    return {
+      ...seat,
+      isBooked: bookedReturnSeat.some(
+        (bookedSeat) => bookedSeat.SeatID === seat.SeatID
+      ),
+    }
+  }) ;
+  const rowsRet = combinedRetSeats.reduce((acc, seat) => {
+    if (seat.SeatCode.includes("A")) {
+      acc.push([seat]);
+    } else {
+      const lastRow = acc[acc.length - 1];
+      lastRow.push(seat);
+    }
+    return acc;
+  }, []);
+  const [id, setId] = useState(0);
+  const [select, setSelect] = useState(0);
   return (
     <>
       <div className="w-full max-w-1000 mx-auto py-5 px-2 flex space-y-3 space-y-reverse sm:space-y-0 sm:space-x-3 flex-col-reverse sm:flex-row">
@@ -55,25 +76,47 @@ export const SeatSelect = ({ params }) => {
           <h1 className="text-2xl  bg-white font-bold text-primary border-b border-primary/20 mb-1 pb-1">
             Seat Number
           </h1>
+        {depSeats.map((seat, index) => {
+          return (<div onClick={() => setId(index)} className="bg-pink-100 w-fit m-1 border p-2 cursor-pointer hover:bg-pink-300">Passenger {index+1}</div>)
+        })}
+          <button onClick={() =>  setSelect(0)} className="border p-2 cursor-pointer rounded hover:bg-blue-300 bg-blue-100 m-1">
+            Departure
+          </button>
+          <button onClick={() =>  setSelect(1)} className="border p-2 cursor-pointer rounded hover:bg-blue-300 bg-blue-100 m-1">
+            Return
+          </button>
           <div className="flex">
             <p className="flex items-center text-m">
-              <TbPointFilled color="orange" size="2.5em" border="solid black" />
-              avaliable
+              <TbPointFilled color="lime" size="2.5em" border="solid black" />
+              available
             </p>
             <p className="flex items-center text-m">
               <TbPointFilled color="gray" size="2.5em" />
-              not avaliable
+              not available
             </p>
           </div>
-          {rows.map((row, index) => {
-            return (
-              <div className="grid grid-cols-13 gap-4 my-4" key={index}>
-                {row.map((seat, seatIndex) => {
-                  return <Seat key={seatIndex} seat={seat} />;
-                })}
-              </div>
-            );
-          })}
+          <span className={select === 0 ? "block" : "hidden"}>
+            {rowsDep.map((row, index) => {
+              return (
+                <div className="grid grid-cols-13 gap-4 my-4" key={index}>
+                  {row.map((seat, seatIndex) => {
+                    return <Seat id={id} key={seatIndex} seat={seat} seatID={depSeats} setSeatID={setDepSeats}/>;
+                  })}
+                </div>
+              );
+            })}
+          </span>
+          <span className={select === 1 ? "block" : "hidden"}>
+            {rowsRet.map((row, index) => {
+              return (
+                <div className="grid grid-cols-13 gap-4 my-4" key={index}>
+                  {row.map((seat, seatIndex) => {
+                    return <Seat id={id} key={seatIndex} seat={seat} seatID={retSeats} setSeatID={setRetSeats}/>;
+                  })}
+                </div>
+              );
+            })}
+          </span>
         </div>
       </div>
     </>
