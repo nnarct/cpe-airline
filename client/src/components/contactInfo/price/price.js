368291a4e6aecd2301a4a15ee5bd6e9c74a997e6
Nannapat Intharasing <nannapat1551@gmail.com>
2023-05-29 15:38:57 +0700

    feat: finish flight price card

--- client/src/components/contactInfo/price/price.js
+++ client/src/components/contactInfo/price/price.js
@@ -2,7 +2,7 @@ import Axios from "axios";
 import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
-export const Price = () => {
+export const Price = ({ protection, addondep, addonret, setTotal }) => {
   const location = useLocation();
   const navigate = useNavigate();
   const params = new URLSearchParams(location.search);
@@ -23,6 +23,21 @@ export const Price = () => {
     navigate("/");
   const [depPrice, setDepPrice] = useState([]);
   const [retPrice, setRetPrice] = useState([]);
+
+  // set base flight price from departure flight price and return flight price
+  const basePrice = flightData.isReturn
+    ? depPrice?.Price + retPrice?.Price
+    : depPrice?.Price;
+
+  const addOnsDepPrice = addondep.reduce((sum, a) => {
+    if (a && typeof a === "object" && "Price" in a) return sum + a.Price;
+    return sum;
+  }, 0); // sum price of all selected add ons for departure flight
+  const addOnsRetPrice = addonret.reduce((sum, a) => {
+    if (a && typeof a === "object" && "Price" in a) return sum + a.Price;
+    return sum;
+  }, 0);
+  const addOnsTotalPrice = addOnsDepPrice + addOnsRetPrice;
   useEffect(() => {
     Axios.post("http://localhost:3001/getPrice", flightData).then(
       (res, err) => {
@@ -34,26 +49,72 @@ export const Price = () => {
         } else console.log(res.data.Error);
       }
     );
-  },[]);
-  const basePrice = flightData.isReturn
-    ? depPrice?.find((p) => p.Class === flightData.Class)?.Price +
-      retPrice?.find((p) => p.Class === flightData.Class)?.Price
-    : depPrice?.find((p) => p.Class === flightData.Class)?.Price;
-    console.log(String(basePrice).replace(/\B(?=(\d{3})+(?!\d))/g, ","))
+    setTotal(addOnsTotalPrice + (protection ? 300 : 0) + basePrice * pass);
+  }, []);
   return (
     <div className="bg-white border border-primary/40 rounded-md py-3 px-3 mt-3">
       <h2 className="text-center font-bold text-2xl border-b">Price</h2>
-      <p className="">Departure Flight</p>
-      <table className="table-fixed">
-        <tr className="text-sm font-bold">
-          <td className="w-full">Adult</td>
-          <td className="whitespace-nowrap">
-            {String(basePrice)?.replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
-              {' x '} {pass} {' '} <span>฿</span>
-          </td>
-        </tr>
-        <tr>Ticket Price</tr>
-      </table>
+      <div className="px-2 text-sm">
+        <div className="flex justify-between font-bold pt-2 pb-2">
+          <span>Adult</span>
+          <span>
+            <span>฿ </span>
+            {basePrice?.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+            {" x "} {pass}
+          </span>
+        </div>
+        <div className="flex justify-between text-gray-600 pb-2">
+          <span>Base fare </span>
+          <span>
+            <span>฿ </span>
+            {(basePrice * pass)
+              .toFixed(2)
+              .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+          </span>
+        </div>
+        {(protection || addOnsTotalPrice > 0) && (
+          <div className="flex justify-between font-bold pt-2 mt-1 border-t">
+            <span>Add-ons</span>
+            <span>
+              <span>฿ </span>
+              {(addOnsTotalPrice + (protection ? 300 : 0))
+                ?.toFixed(2)
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+            </span>
+          </div>
+        )}
+        {addOnsTotalPrice > 0 && (
+          <div className="flex justify-between text-gray-600 pt-2 mb-1">
+            <span>Checked baggage</span>
+            <span>
+              <span>฿ </span>
+              {addOnsTotalPrice
+                .toFixed(2)
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+            </span>
+          </div>
+        )}
+        {protection === true && (
+          <div className="flex justify-between text-gray-600 pt-2 mb-2">
+            <span>Trip protection</span>
+            <span>
+              <span>฿ </span>
+              300
+            </span>
+          </div>
+        )}
+        {
+          <div className="flex justify-between font-bold pt-2 mt-1 border-t">
+            <span>Total</span>
+            <span>
+              <span>฿ </span>
+              {(addOnsTotalPrice + (protection ? 300 : 0) + basePrice * pass)
+                ?.toFixed(2)
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+            </span>
+          </div>
+        }
+      </div>
     </div>
   );
 };
