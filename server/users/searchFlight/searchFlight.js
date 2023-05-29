09b4adf203f798eb1c08a8aa62d87619baf9a715
Nannapat Intharasing <nannapat1551@gmail.com>
2023-05-29 16:09:00 +0700

    fix: select only flight that class is not null

--- server/users/searchFlight/searchFlight.js
+++ server/users/searchFlight/searchFlight.js
@@ -2,7 +2,7 @@ import { db } from "../../index.js";
 
 export const searchFlights = (req, res) => {
   const sql =
-    "SELECT f.FlightID, f.FlightNumber, f.AirlineID, al.Name AS AirlineName, f.DepartureTime, f.ArrivalTime, oa.Name AS Origin, oa.IATA AS OriIATA, da.Name AS Destination, da.IATA AS DesIATA, f.PlaneID, p.Price FROM flight AS f INNER JOIN airport AS oa ON oa.AirportID = f.OriginAirportID INNER JOIN airport AS da ON da.AirportID = f.DestinationAirportID INNER JOIN airline AS al ON al.AirlineID = f.AirlineID LEFT JOIN price AS p ON p.FlightID = f.FlightID LEFT JOIN class AS c ON c.ClassID = p.ClassID AND c.Name = ? WHERE f.OriginAirportID = ? AND f.DestinationAirportID = ? AND f.DepartureTime LIKE ?";
+    "SELECT f.FlightID, f.FlightNumber, f.AirlineID, al.Name AS AirlineName, f.DepartureTime, f.ArrivalTime, oa.Name AS Origin, oa.IATA AS OriIATA, da.Name AS Destination, da.IATA AS DesIATA, f.PlaneID, p.Price, c.Name AS class FROM flight AS f INNER JOIN airport AS oa ON oa.AirportID = f.OriginAirportID INNER JOIN airport AS da ON da.AirportID = f.DestinationAirportID INNER JOIN airline AS al ON al.AirlineID = f.AirlineID LEFT JOIN price AS p ON p.FlightID = f.FlightID LEFT JOIN class AS c ON c.ClassID = p.ClassID AND c.Name = ? WHERE f.OriginAirportID = ? AND f.DestinationAirportID = ? AND f.DepartureTime LIKE ? AND c.Name IS NOT NULL";
   db.query(
     sql,
     [req.body.class, req.body.from, req.body.to, `${req.body.departure}%`],
