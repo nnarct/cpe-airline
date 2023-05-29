7fb717cbfd32cd8c59a2242f8f3c9122a58db0c9
Nannapat Intharasing <nannapat1551@gmail.com>
2023-05-30 02:58:38 +0700

    ref: remove console log

--- server/users/myFlight/getBookingInfo.js
+++ server/users/myFlight/getBookingInfo.js
@@ -3,7 +3,6 @@ export const getBookingInfo = (req, res) =>{
    
     const sql = 
     "SELECT b.ContactFirstname,b.ContactLastname,b.ContactPhone,b.ContactEmail,p.FirstName,p.LastName,i.Class,s.SeatCode,ad.Weight FROM passenger p INNER JOIN booking b  ON b.BookingID=p.BookingID inner JOIN invoice i ON b.InvoiceID=i.InvoiceID left JOIN seat s ON p.SeatID=s.SeatID left JOIN addons ad ON p.AddOnsID=ad.AddOnsID WHERE b.BookingID=?";
-    console.log(req.body)
     db.query(sql, [req.body.id], (err, data) => {
         if (err){
             return res.json({ Error: "Error while get booking info..." });
