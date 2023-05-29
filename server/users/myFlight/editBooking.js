f1b9de4d1909e5ab7431ed61d2c6375f344a3c0b
Nannapat Intharasing <nannapat1551@gmail.com>
2023-05-30 04:10:16 +0700

    fix

--- server/users/myFlight/editBooking.js
+++ server/users/myFlight/editBooking.js
@@ -4,7 +4,7 @@ export const editBooking = (req, res) => {
     "UPDATE booking SET ContactFirstname=?,ContactLastname=?,ContactPhone=?,ContactEmail=? WHERE BookingID=?";
   db.query(
     sql,
-    [re.body.fname, req.body.lname, req.body.email, req.body.telno],
+    [req.body.fname, req.body.lname, req.body.email, req.body.telno],
     (err, data) => {
       if (err) {
         return res.json({ Error: "Error while editing contact..." });
@@ -16,7 +16,7 @@ export const editBooking = (req, res) => {
 export const editPassenger = (req, res) => {
   const sql =
     "UPDATE passenger SET FirstName=?,DOB = ?,Gender = ?,LastName=?, Nationality = ? WHERE PassengerID=?";
-  db.query(sql, [re.body.fname, req.body.lname, req.body.id], (err, data) => {
+  db.query(sql, [req.body.fname, req.body.lname, req.body.id], (err, data) => {
     if (err) {
       return res.json({ Error: "Error while editing passenger..." });
     } else return res.json({ Error: "No booking info found." });
