60e3e4048dd93a8730829174f49cec51f4c673fe
Nannapat Intharasing <nannapat1551@gmail.com>
2023-05-29 13:40:12 +0700

    feat: retrieve class of price by class id

--- server/users/createBooking/getPrice.js
+++ server/users/createBooking/getPrice.js
@@ -1,21 +1,27 @@
 import { db } from "../../index.js";
 
 export const getPrice = (req, res) => {
-  const sql = "SELECT * FROM price WHERE FlightID = ?";
+  const sql =
+    "SELECT p.PriceID, p.Price, p.FlightID, p.ClassID, c.Name AS Class FROM price p INNER JOIN class c ON c.ClassID = p.ClassID WHERE FlightID = ?";
   db.query(sql, [req.body.DepartureFlightID], (err, dep) => {
-    if (err) return res.json({ Error: "Error while finding departure flight price in db..." });
+    if (err)
+      return res.json({
+        Error: "Error while finding departure flight price in db...",
+      });
     if (dep.length > 0) {
       if (req.body.isReturn === "1" || req.body.isReturn === 1) {
         db.query(sql, [req.body.ReturnFlightID], (err, ret) => {
           if (err)
-            return res.json({ Error: "Error while finding return flight price in db..." ,  DepartureFlightPrice: dep });
-          if (ret.length > 0) {
+            return res.json({
+              Error: "Error while finding return flight price in db...",
+              DepartureFlightPrice: dep,
+            });
+          if (ret.length > 0)
             return res.json({
               Status: "Success",
               DepartureFlightPrice: dep,
               ReturnFlightPrice: ret,
             });
-          }
           return res.json({
             Status: "Success",
             DepartureFlightPrice: dep,
@@ -23,10 +29,7 @@ export const getPrice = (req, res) => {
             Error: "No return flight price found.",
           });
         });
-      } else {
-        return res.json({ Status: "Success", DepartureFlightPrice: dep });
-      }
+      } else return res.json({ Status: "Success", DepartureFlightPrice: dep });
     }
-    return res.json({ Error: "No departure flight price found." });
   });
 };
