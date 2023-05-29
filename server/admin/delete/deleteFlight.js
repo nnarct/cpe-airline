f7211b954798f18d9ba7d408a4c84a9b024d74d3
Nannapat Intharasing <nannapat1551@gmail.com>
2023-05-30 02:57:53 +0700

    ref:

--- server/admin/delete/deleteFlight.js
+++ server/admin/delete/deleteFlight.js
@@ -3,6 +3,7 @@ export const deleteFlight = (req, res) => {
   const sql = "DELETE FROM flight WHERE FlightID = ?";
   db.query(sql, [req.body.id], (err, result) => {
     if (err) return res.json({ Error: "Error while deleting flight..." });
-    return res.json({ Status: "Deleteflight successfully! :)" });
+    return res.json({ Status: "Delete flight successfully! :)" });
   });
 };
+// ON DELETE CASCADE
