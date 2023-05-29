fc184ea78fca79c45186fabe5f9886eb456680e3
Nannapat Intharasing <nannapat1551@gmail.com>
2023-05-29 11:35:45 +0700

    style: change div group for price card

--- client/src/components/contactInfo/contactInfo.js
+++ client/src/components/contactInfo/contactInfo.js
@@ -30,6 +30,7 @@ import {
   handleValueChange,
   handleGender,
 } from "./functions";
+import { Price } from "./price/price";
 
 // Todo - restrict access to logged in users only
 
@@ -140,222 +141,227 @@ export const ContactInfo = () => {
     <>
       <Navbar />
       <PageWrapper>
-        <div className="w-full max-w-1000 mx-auto pt-3 px-2 flex space-y-3 space-y-reverse sm:space-y-0 sm:space-x-3 flex-col-reverse sm:flex-row">
-          <ContactSection>
-            <form action="" ref={info}>
-              <InputFrom label="First Name">
-                <input
-                  name="cfirstname"
-                  type="text"
-                  className={inp}
-                  defaultValue={contact.FirstName}
-                  onChange={(e) =>
-                    setContact({ ...contact, FirstName: e.target.value })
-                  }
-                />
-              </InputFrom>
-              <InputFrom label="Last Name">
-                <input
-                  name="clastname"
-                  type="text"
-                  className={inp}
-                  defaultValue={contact.LastName}
-                  onChange={(e) =>
-                    setContact({ ...contact, LastName: e.target.value })
-                  }
-                />
-              </InputFrom>
-              <InputFrom label="Phone">
-                <input
-                  name="cphone"
-                  type="text"
-                  className={inp}
-                  defaultValue={contact.phone}
-                  onChange={(e) =>
-                    setContact({ ...contact, phone: e.target.value })
-                  }
-                />
-              </InputFrom>
-              <InputFrom label="Email">
-                <input
-                  name="cemail"
-                  type="text"
-                  className={inp}
-                  defaultValue={contact.email}
-                  onChange={(e) =>
-                    setContact({ ...contact, email: e.target.value })
-                  }
-                />
-              </InputFrom>
-              {pass?.map((t, i) => {
-                return (
-                  <PassengerInfo type={t} no={i + 1} key={i}>
-                    <InputFrom label="First Name">
-                      <input
-                        name={`fname${i + 1}`}
-                        type="text"
-                        className={inp}
-                        defaultValue={value[i]?.FirstName}
-                      />
-                    </InputFrom>
-                    <InputFrom label="Last Name">
-                      <input
-                        name={`lname${i + 1}`}
-                        type="text"
-                        className={inp}
-                        defaultValue={value[i]?.LastName}
-                      />
-                    </InputFrom>
-                    <InputFrom label="Nationality">
-                      <input
-                        name={`nationality${i + 1}`}
-                        type="text"
-                        className={inp}
-                        defaultValue={value[i]?.nationality}
-                      />
-                    </InputFrom>
-                    <InputFrom label="Date of birth">
-                      <Datepicker
-                        asSingle={true}
-                        useRange={false}
-                        minDate={new Date("1900-01-02")}
-                        maxDate={new Date()}
-                        value={
-                          value[i]
-                            ? {
-                                startDate: value[i].dob.startDate,
-                                endDate: value[i].dob.endDate,
-                              }
-                            : null
-                        }
-                        placeholder="DD-MM-YYYY"
-                        displayFormat={"DD-MM-YYYY"}
-                        inputClassName={inp}
-                        onChange={(e) =>
-                          handleValueChange(i + 1, e, value, setValue)
-                        }
-                      />
-                      <label
-                        id={`gender${i + 1}`}
-                        className="px-3 whitespace-nowrap text-primary"
-                      >
-                        Gender
-                      </label>
-                      <span className="flex space-x-2 pr-3">
+        <div className="flex max-w-1000 bg-blue mx-auto">
+          <div className="sm:w-3/5 md:w-4/6 pl-2 pr-1 flex flex-col">
+            <ContactSection>
+              <form action="" ref={info}>
+                <InputFrom label="First Name">
+                  <input
+                    name="cfirstname"
+                    type="text"
+                    className={inp}
+                    defaultValue={contact.FirstName}
+                    onChange={(e) =>
+                      setContact({ ...contact, FirstName: e.target.value })
+                    }
+                  />
+                </InputFrom>
+                <InputFrom label="Last Name">
+                  <input
+                    name="clastname"
+                    type="text"
+                    className={inp}
+                    defaultValue={contact.LastName}
+                    onChange={(e) =>
+                      setContact({ ...contact, LastName: e.target.value })
+                    }
+                  />
+                </InputFrom>
+                <InputFrom label="Phone">
+                  <input
+                    name="cphone"
+                    type="text"
+                    className={inp}
+                    defaultValue={contact.phone}
+                    onChange={(e) =>
+                      setContact({ ...contact, phone: e.target.value })
+                    }
+                  />
+                </InputFrom>
+                <InputFrom label="Email">
+                  <input
+                    name="cemail"
+                    type="text"
+                    className={inp}
+                    defaultValue={contact.email}
+                    onChange={(e) =>
+                      setContact({ ...contact, email: e.target.value })
+                    }
+                  />
+                </InputFrom>
+                {pass?.map((t, i) => {
+                  return (
+                    <PassengerInfo type={t} no={i + 1} key={i}>
+                      <InputFrom label="First Name">
                         <input
-                          type="radio"
-                          htmlFor="gender"
-                          name={`gender${i + 1}`}
-                          onClick={() =>
-                            handleGender(i + 1, "female", genders, setGenders)
+                          name={`fname${i + 1}`}
+                          type="text"
+                          className={inp}
+                          defaultValue={value[i]?.FirstName}
+                        />
+                      </InputFrom>
+                      <InputFrom label="Last Name">
+                        <input
+                          name={`lname${i + 1}`}
+                          type="text"
+                          className={inp}
+                          defaultValue={value[i]?.LastName}
+                        />
+                      </InputFrom>
+                      <InputFrom label="Nationality">
+                        <input
+                          name={`nationality${i + 1}`}
+                          type="text"
+                          className={inp}
+                          defaultValue={value[i]?.nationality}
+                        />
+                      </InputFrom>
+                      <InputFrom label="Date of birth">
+                        <Datepicker
+                          asSingle={true}
+                          useRange={false}
+                          minDate={new Date("1900-01-02")}
+                          maxDate={new Date()}
+                          value={
+                            value[i]
+                              ? {
+                                  startDate: value[i].dob.startDate,
+                                  endDate: value[i].dob.endDate,
+                                }
+                              : null
+                          }
+                          placeholder="DD-MM-YYYY"
+                          displayFormat={"DD-MM-YYYY"}
+                          inputClassName={inp}
+                          onChange={(e) =>
+                            handleValueChange(i + 1, e, value, setValue)
                           }
                         />
-                        <span>female</span>
+                        <label
+                          id={`gender${i + 1}`}
+                          className="px-3 whitespace-nowrap text-primary"
+                        >
+                          Gender
+                        </label>
+                        <span className="flex space-x-2 pr-3">
+                          <input
+                            type="radio"
+                            htmlFor="gender"
+                            name={`gender${i + 1}`}
+                            onClick={() =>
+                              handleGender(i + 1, "female", genders, setGenders)
+                            }
+                          />
+                          <span>female</span>
+                        </span>
+                        <span className="flex space-x-2 pr-3">
+                          <input
+                            type="radio"
+                            htmlFor="gender"
+                            name={`gender${i + 1}`}
+                            onClick={() =>
+                              handleGender(i + 1, "male", genders, setGenders)
+                            }
+                            className="p-1"
+                          />
+                          <span>male</span>
+                        </span>
+                      </InputFrom>
+                    </PassengerInfo>
+                  );
+                })}
+              </form>
+            </ContactSection>
+            {/* Trip Protection */}
+            <AddOns contact={contact} setContact={setContact} />
+            {/* Baggage */}
+            <ExtWrap>
+              <div className="border-b border-primary/20 mb-1 pb-1 flex ">
+                <h1 className="text-2xl font-bold text-primary ">Baggage</h1>
+
+                {flightData.isReturn ? (
+                  <>
+                    <div
+                      className={`cursor-pointer text-xs ml-4 w-36 text-center justify-center text-gray-600 flex items-center hover:bg-slate-100 cursor-pointer flex flex-col py-1 ${
+                        baggageDate === 1 && "bg-slate-100"
+                      }`}
+                      onClick={() => setBaggageDate(1)}
+                    >
+                      <span>
+                        {moment(dep.DepartureTime).format("DD MMM YYYY")}
                       </span>
-                      <span className="flex space-x-2 pr-3">
-                        <input
-                          type="radio"
-                          htmlFor="gender"
-                          name={`gender${i + 1}`}
-                          onClick={() =>
-                            handleGender(i + 1, "male", genders, setGenders)
-                          }
-                          className="p-1"
-                        />
-                        <span>male</span>
+                      <span className="flex items-center">
+                        {dep.OriIATA} <BsArrowRightShort /> {dep.DesIATA}
                       </span>
-                    </InputFrom>
-                  </PassengerInfo>
-                );
-              })}
-            </form>
-          </ContactSection>
-          <div className="flex flex-col sm:w-2/5 md:w-2/6 m-0 px-3 w-full">
+                    </div>
+                    <div
+                      className={`cursor-pointer border-l w-36 text-xs text-center text-gray-600 flex justify-center items-center hover:bg-slate-100 cursor-pointer flex flex-col py-1 ${
+                        baggageDate === 2 && "bg-slate-100"
+                      }`}
+                      onClick={() => setBaggageDate(2)}
+                    >
+                      <span>
+                        {moment(ret.DepartureTime).format("DD MMM YYYY")}
+                      </span>
+                      <span className="flex items-center">
+                        {ret.OriIATA} <BsArrowRightShort /> {ret.DesIATA}
+                      </span>
+                    </div>
+                  </>
+                ) : null}
+              </div>
+              <div className={baggageDate === 1 ? "block" : "hidden"}>
+                {pass?.map((p, i) => {
+                  return (
+                    <>
+                      <div className="flex items-center justify-between">
+                        <h1 className="mt-1 text-lg font-medium text-gray-600">
+                          Passenger {i + 1} ({p})
+                        </h1>
+                      </div>
+                      <BaseLuggage base={base?.DepBase} />
+                      <LuggagesDep
+                        luggages={addon.dep}
+                        id={i}
+                        fn={handleDepAdd}
+                        addondep={addondep}
+                      />
+                    </>
+                  );
+                })}
+              </div>
+              <div className={baggageDate === 2 ? "block" : "hidden"}>
+                {pass?.map((p, i) => {
+                  return (
+                    <>
+                      <div className="flex items-center justify-between">
+                        <h1 className="mt-1 text-lg font-medium text-gray-600">
+                          Passenger {i + 1} ({p})
+                        </h1>
+                      </div>
+                      <BaseLuggage base={base?.RetBase} />
+                      <LuggagesRet
+                        luggages={addon.ret}
+                        id={i}
+                        fn={handleRetAdd}
+                        addonret={addonret}
+                      />
+                    </>
+                  );
+                })}
+              </div>
+            </ExtWrap>
+          </div>
+          <div className="flex flex-col sm:w-2/5 md:w-2/6 m-0 px-2 w-full">
             <FlightInfo
               pass={
                 Number(data.adult) + Number(data.child) + Number(data.infant)
               }
               classType={data.c}
             />
-            <div>Price info</div>
+            <Price />
           </div>
         </div>
-        {/* Trip Protection */}
-        <AddOns contact={contact} setContact={setContact} />
-        {/* Baggage */}
-        <ExtWrap>
-          <div className="border-b border-primary/20 mb-1 pb-1 flex ">
-            <h1 className="text-2xl font-bold text-primary ">Baggage</h1>
-
-            {flightData.isReturn ? (
-              <>
-                <div
-                  className={`cursor-pointer text-xs ml-4 w-36 text-center justify-center text-gray-600 flex items-center hover:bg-slate-100 cursor-pointer flex flex-col py-1 ${
-                    baggageDate === 1 && "bg-slate-100"
-                  }`}
-                  onClick={() => setBaggageDate(1)}
-                >
-                  <span>{moment(dep.DepartureTime).format("DD MMM YYYY")}</span>
-                  <span className="flex items-center">
-                    {dep.OriIATA} <BsArrowRightShort /> {dep.DesIATA}
-                  </span>
-                </div>
-                <div
-                  className={`cursor-pointer border-l w-36 text-xs text-center text-gray-600 flex justify-center items-center hover:bg-slate-100 cursor-pointer flex flex-col py-1 ${
-                    baggageDate === 2 && "bg-slate-100"
-                  }`}
-                  onClick={() => setBaggageDate(2)}
-                >
-                  <span>{moment(ret.DepartureTime).format("DD MMM YYYY")}</span>
-                  <span className="flex items-center">
-                    {ret.OriIATA} <BsArrowRightShort /> {ret.DesIATA}
-                  </span>
-                </div>
-              </>
-            ) : null}
-          </div>
-          <div className={baggageDate === 1 ? "block" : "hidden"}>
-            {pass?.map((p, i) => {
-              return (
-                <>
-                  <div className="flex items-center justify-between">
-                    <h1 className="mt-1 text-lg font-medium text-gray-600">
-                      Passenger {i + 1} ({p})
-                    </h1>
-                  </div>
-                  <BaseLuggage base={base?.DepBase} />
-                  <LuggagesDep
-                    luggages={addon.dep}
-                    id={i}
-                    fn={handleDepAdd}
-                    addondep={addondep}
-                  />
-                </>
-              );
-            })}
-          </div>
-          <div className={baggageDate === 2 ? "block" : "hidden"}>
-            {pass?.map((p, i) => {
-              return (
-                <>
-                  <div className="flex items-center justify-between">
-                    <h1 className="mt-1 text-lg font-medium text-gray-600">
-                      Passenger {i + 1} ({p})
-                    </h1>
-                  </div>
-                  <BaseLuggage base={base?.RetBase} />
-                  <LuggagesRet
-                    luggages={addon.ret}
-                    id={i}
-                    fn={handleRetAdd}
-                    addonret={addonret}
-                  />
-                </>
-              );
-            })}
-          </div>
-        </ExtWrap>
-
         {/* seat */}
         <SeatSelect
           params={params}
@@ -367,6 +373,7 @@ export const ContactInfo = () => {
           ret={ret}
           dep={dep}
         />
+
         <button
           type="submit"
           onClick={handleSubmit}
