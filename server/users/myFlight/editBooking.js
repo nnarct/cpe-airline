70658ff8b12b3820b96e939e84518ef1c15edd90
Nannapat Intharasing <nannapat1551@gmail.com>
2023-05-30 17:50:39 +0700

    ref: make code easy to read

--- server/users/myFlight/editBooking.js
+++ server/users/myFlight/editBooking.js
@@ -1,24 +1,47 @@
 import { db } from "../../index.js";
 export const editBooking = (req, res) => {
-  const sql =
-    "UPDATE booking SET ContactFirstname=?,ContactLastname=?,ContactPhone=?,ContactEmail=? WHERE BookingID=?";
+  const sql = `
+    UPDATE
+      booking
+    SET
+      ContactFirstname = ?,
+      ContactLastname = ?,
+      ContactPhone = ?,
+      ContactEmail = ?
+    WHERE
+      BookingID = ? ; `;
+
   db.query(
     sql,
-    [req.body.fname, req.body.lname, req.body.email, req.body.telno],
+    [
+      req.body.fname,
+      req.body.lname,
+      req.body.telno,
+      req.body.email,
+      req.body.id,
+    ],
     (err, data) => {
-      if (err) {
-        return res.json({ Error: "Error while editing contact..." });
-      } else return res.json({ Error: "No booking info found." });
+      if (err) return res.json({ Error: "Error while editing contact..." });
+      else return res.json({ Status: "Edit contact successfully." });
     }
   );
 };
 
-export const editPassenger = (req, res) => {
-  const sql =
-    "UPDATE passenger SET FirstName=?,DOB = ?,Gender = ?,LastName=?, Nationality = ? WHERE PassengerID=?";
-  db.query(sql, [req.body.fname, req.body.lname, req.body.id], (err, data) => {
-    if (err) {
-      return res.json({ Error: "Error while editing passenger..." });
-    } else return res.json({ Error: "No booking info found." });
-  });
+export const editPassengerInBooking = (req, res) => {
+  const sql = `UPDATE 
+        passenger 
+    SET 
+        FirstName = ? ,
+        LastName = ?,
+        Nationality = ?,
+    WHERE
+        PassengerID = ? ; `;
+  db.query(
+    sql,
+    [req.body.fname, req.body.lname, req.body.nationality, req.body.id],
+    (err, response) => {
+      if (err) return res.json({ Error: "Error while editing passenger..." });
+      else return res.json({ Status: "Edit passenger successfully." });
+    }
+  );
 };
