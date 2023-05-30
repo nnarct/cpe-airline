db4c711514fc5b15bd030f79c14efdf3e521381a
Nannapat Intharasing <nannapat1551@gmail.com>
2023-05-30 17:48:22 +0700

    ref: make the code easy to read

--- server/users/myFlight/cancelBooking.js
+++ server/users/myFlight/cancelBooking.js
@@ -1,13 +1,13 @@
 import { db } from "../../index.js";
 export const cancelBooking = (req, res) => {
-  const booking = "UPDATE Booking SET Status = 0 WHERE BookingID=?";
-  const passenger = "DELETE FROM Passenger WHERE BookingID=?";
+  const booking = "UPDATE Booking SET Status = 0 WHERE BookingID = ?";
+  const passenger = "DELETE FROM Passenger WHERE BookingID = ?";
   db.query(booking, [req.body.id], (err, data) => {
     if (err) {
       return res.json({ Error: "Error while cancel booking..." });
     }
     db.query(passenger, [req.body.id], (err, data) => {
-      if (err) return res.json({ Error: "Error while deleting passenger..." });
+      if (err) return res.json({ Error: "Error while deleting passenger data..." });
       else return res.json({ Status: "Cancel booking successfully! :)" });
     });
   });
